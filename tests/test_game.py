d9b42adb7591caf6956e7cc610d7eab59b8b757d
BetterBuiltFool <betterbuiltfool@gmail.com>
2024-12-25 14:09:02 -0500

    Adds test framework

---
+++ tests/test_game.py
@@ -0,0 +1,18 @@
+import unittest
+
+from src.simple_game.game import Game
+
+
+class Test(Game):
+
+    pass
+
+
+class TestGame(unittest.TestCase):
+
+    pass
+
+
+if __name__ == "__main__":
+
+    unittest.main()
