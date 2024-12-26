2e75eb37c2397e6983f38b509caa6e67acfd7dbb
BetterBuiltFool <betterbuiltfool@gmail.com>
2024-12-26 12:55:41 -0500

    Updates docstrings for screen_data

--- src/simple_game/screen_data.py
+++ src/simple_game/screen_data.py
@@ -13,15 +13,55 @@ class ResolutionData:
         vsync: int = 0,
         **kwds
     ) -> None:
-        self.resolution = size
-        self.flags = flags
-        self.display = display
-        self.vsync = vsync
-        self.is_fullscreen = (self.flags & pygame.FULLSCREEN) or kwds.get(
+        """
+        Creates a ResolutionData object.
+
+        :param size: A Point-like object representing the width and height of the
+        window, defaults to (800, 600)
+        :param flags: Bitmask of pygame display flags, defaults to 0 (default pygame
+        flags)
+        :param display: Index of display used for set_mode
+        :param vsync: Flag for enabling vsync, defaults to 0 (off)
+        :param fullscreen: Boolean to force fullscreen mode (keyword-only)
+        """
+        self.is_fullscreen = (flags & pygame.FULLSCREEN) or kwds.get(
             "fullscreen", False
         )
+        """
+        Flag indicating that the game is operating in fullscreen mode.
+        """
+        if self.is_fullscreen:
+            # Ensure the fullscreen flag is included if the fullscreen parameter is.
+            flags = flags | pygame.FULLSCREEN
+        self.resolution = size
+        """
+        A Point-like object representing the width and height of the window.
+        """
+        self.flags = flags
+        """
+        Bitmask of pygame display flags
+        """
+        self.display = display
+        """
+        Index of display used for set_mode
+        """
+        self.vsync = vsync
+        """
+        Flag for enabling vsync.
+
+        0 = Off
+
+        1 = On
+
+        -1 = Adaptive vsync (requires OpenGL flag)
+        """
 
     def rescale_window(self) -> pygame.Surface:
+        """
+        Updates the window based on the stored state.
+
+        :return: A new surface representing the display.
+        """
         try:
             window_surface = pygame.display.set_mode(
                 self.resolution, self.flags, 0, self.display, self.vsync
