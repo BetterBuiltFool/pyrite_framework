39d160cd104c95c41bad05082317a61942836667
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-01-01 10:34:45 -0500

    Makes game_instance an attribute of _base_type

--- src/pyrite/types/_base_type.py
+++ src/pyrite/types/_base_type.py
@@ -1,12 +1,17 @@
 from abc import ABC
+import logging
 
 # from ..game import get_game_instance
 import src.pyrite.game as game
 
 
+logger = logging.getLogger(__name__)
+
+
 class _BaseType(ABC):
 
     def __init__(self) -> None:
+        self.game_instance: game.Game = None
         self._enabled: bool = True
         self.enabled = True
 
@@ -17,8 +22,12 @@ class _BaseType(ABC):
     @enabled.setter
     def enabled(self, value: bool) -> None:
         self._enabled = value
-        game_instance = game.get_game_instance()
+        if self.game_instance is None:
+            self.game_instance = game.get_game_instance()
+        if self.game_instance is None:
+            logger.warning("No running game instance available.")
+            return
         if value:
-            game_instance.enable(self)
+            self.game_instance.enable(self)
         else:
-            game_instance.disable(self)
+            self.game_instance.disable(self)
