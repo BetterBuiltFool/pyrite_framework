aefa7822b05410bd865705c9d3d4afb5c3847312
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-01-01 13:51:14 -0500

    Adds important classes to package-level Defines the API

--- src/pyrite/__init__.py
+++ src/pyrite/__init__.py
@@ -1,6 +1,11 @@
 import pygame
 
-from ._data_classes.display_settings import DisplaySettings  # noqa:F401
+from ._data_classes.display_settings import DisplaySettings
+from ._data_classes.metadata import Metadata  # noqa:F401
+from ._data_classes.timing_settings import TimingSettings  # noqa:F401
+from .game import Game, AsyncGame  # noqa:F401
+from .types.entity import Entity  # noqa:F401
+from .types.renderable import Renderable, UIElement  # noqa:F401
 
 DEFAULT_WINDOWED: DisplaySettings = DisplaySettings()
 """
