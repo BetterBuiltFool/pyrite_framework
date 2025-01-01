55f2e1a5d9fdeccb9b14c0bd9b8fc97385f759ac
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-01-01 13:10:47 -0500

    Makes render and render_ui properly abstract

--- src/pyrite/types/renderable.py
+++ src/pyrite/types/renderable.py
@@ -1,18 +1,18 @@
-from abc import abstractmethod
+from abc import abstractmethod, ABC
 
 from ._base_type import _BaseType
 
 import pygame
 
 
-class Renderable(_BaseType):
+class Renderable(_BaseType, ABC):
 
     @abstractmethod
     def render(self, delta_time: float) -> tuple[pygame.Surface, pygame.Rect]:
         pass
 
 
-class UIElement(_BaseType):
+class UIElement(_BaseType, ABC):
 
     @abstractmethod
     def render_ui(self, delta_time: float) -> tuple[pygame.Surface, pygame.Rect]:
