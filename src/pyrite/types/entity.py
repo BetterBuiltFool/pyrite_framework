2730399303ab0b31ab70c93d2aa83b04b170a519
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-01-01 13:43:33 -0500

    Updates docstrings

--- src/pyrite/types/entity.py
+++ src/pyrite/types/entity.py
@@ -2,15 +2,48 @@ from ._base_type import _BaseType
 
 
 class Entity(_BaseType):
+    """
+    Base class for any class that exhibits behaviour during any of the update phases.
+    """
 
     def pre_update(self, delta_time: float) -> None:
+        """
+        A method that is called during the pre_update phase.
+        Will always be called before update.
+
+        :param delta_time: Time passed since last frame.
+        """
         pass
 
     def update(self, delta_time: float) -> None:
+        """
+        A method that is called during the main update phase.
+        Most behaviour should happen here.
+
+        :param delta_time: Time passed since last frame.
+        """
         pass
 
     def post_update(self, delta_time: float) -> None:
+        """
+        A method that is called during the post_update phase.
+        Will always be called after update.
+
+        :param delta_time: Time passed since last frame.
+        """
         pass
 
     def const_update(self, delta_time: float) -> None:
+        """
+        A method that is called during the const_update phase.
+        Useful for behavior that is sensitive to time fluctuations,
+        such as physics or AI.
+
+        const_update is called before any other update methods.
+
+        const_update may be called any number of times per frame,
+        depending on timestep length.
+
+        :param delta_time: Length of the timestep being simulated.
+        """
         pass
