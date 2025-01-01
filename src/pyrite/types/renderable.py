083e4191a7134bf91299d68c4359f951fd4e1185
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-01-01 13:29:41 -0500

    Updates docstrings

--- src/pyrite/types/renderable.py
+++ src/pyrite/types/renderable.py
@@ -6,9 +6,20 @@ import pygame
 
 
 class Renderable(_BaseType, ABC):
+    """
+    Base class for any renderable object in pyrite.
+    """
 
     @abstractmethod
     def render(self, delta_time: float) -> tuple[pygame.Surface, pygame.Rect]:
+        """
+        Supplies a surface ready to be blitted to another surface.
+
+        :param delta_time: Time passed since last frame. Can be ignored by the concrete
+        method but must be accepted.
+        :return: A tuple containing a ready-to-draw surface and a point with relative
+        location
+        """
         pass
 
 
@@ -16,4 +27,13 @@ class UIElement(_BaseType, ABC):
 
     @abstractmethod
     def render_ui(self, delta_time: float) -> tuple[pygame.Surface, pygame.Rect]:
+        """
+        Supplies a surface ready to be blitted to another surface. Drawn late during
+        the render_ui phase, so it will always be drawn over any other renderings.
+
+        :param delta_time: Time passed since last frame. Can be ignored by the concrete
+        method but must be accepted.
+        :return: A tuple containing a ready-to-draw surface and a point with relative
+        location
+        """
         pass
