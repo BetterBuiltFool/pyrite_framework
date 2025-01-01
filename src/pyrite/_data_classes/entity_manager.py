002b824928167074253bd5b12489b27162473276
BetterBuiltFool <betterbuiltfool@gmail.com>
2024-12-31 19:59:41 -0500

    Implements EntityManager

--- src/pyrite/_data_classes/entity_manager.py
+++ src/pyrite/_data_classes/entity_manager.py
@@ -1,3 +1,21 @@
+from weakref import WeakSet
+
+from src.pyrite.types.entity import Entity
+from src.pyrite.types.renderable import Renderable
+
+
 class EntityManager:
 
-    pass
+    def __init__(self) -> None:
+        self.updateables: WeakSet[Entity] = WeakSet()
+        self.renderables: WeakSet[Renderable] = WeakSet()
+
+    def enable(self, item: Entity | Renderable) -> None:
+        if isinstance(item, Entity):
+            self.updateables.add(item)
+        if isinstance(item, Renderable):
+            self.renderables.add(item)
+
+    def disable(self, item: Entity | Renderable) -> None:
+        self.updateables.discard(item)
+        self.renderables.discard(item)
