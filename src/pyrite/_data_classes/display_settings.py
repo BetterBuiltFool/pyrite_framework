464a3276197bdaf0728fbd8dd404f68b3851c46f
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-01-01 19:16:57 -0500

    Fixes vsync being set to False instead of 0

--- src/pyrite/_data_classes/display_settings.py
+++ src/pyrite/_data_classes/display_settings.py
@@ -70,7 +70,7 @@ class DisplaySettings:
                 display_settings.resolution,
                 display_settings.flags,
                 display_settings.display,
-                vsync=False,
+                vsync=0,
             )
             display_settings = new_settings
             window_surface = DisplaySettings._create_window(display_settings)
