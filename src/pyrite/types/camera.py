5a30bc84ef8ba8c9d61e6b1937a7f9c2246cf06c
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-01-04 17:11:26 -0500

    Adds start to camera

---
+++ src/pyrite/types/camera.py
@@ -0,0 +1,63 @@
+from abc import ABC
+from collections.abc import Iterable
+
+
+from src.pyrite.types.renderable import Renderable
+from src.pyrite.types.enums import RenderLayers
+
+import pygame
+
+
+class CameraBase(ABC):
+    """
+    Defines the important attributes of a camera for the sake of drawing onto its
+    surface.
+
+    Can be constructed from the window.
+    """
+
+    def __init__(
+        self,
+        surface: pygame.Surface,
+        position: pygame.typing.Point = None,
+    ) -> None:
+        self.surface = surface
+        self.viewport = surface.get_rect()
+        """
+        A rectangle representing the actual viewable area of the camera
+        """
+        if position is None:
+            position = self.viewport.topleft
+        self.position = position
+
+    def cull(
+        self, delta_time: float, items: Iterable[Renderable]
+    ) -> Iterable[tuple[Renderable, pygame.Rect]]:
+        return (item.render(delta_time) for item in items)
+
+
+class Camera(CameraBase, Renderable):
+    """
+    Basic form of a camera that is capable of rendering to the screen.
+    """
+
+    def __init__(
+        self,
+        max_size: pygame.typing.RectLike,
+        position: pygame.typing.Point = None,
+        game_instance=None,
+        enabled=True,
+        draw_index=0,
+    ) -> None:
+        max_size = pygame.Rect(max_size)
+        surface = pygame.Surface(max_size.size)
+        CameraBase.__init__(self, max_size, surface, position)
+        Renderable.__init__(
+            self, game_instance, enabled, RenderLayers.CAMERA, draw_index
+        )
+
+    def get_rect(self) -> pygame.Rect:
+        return self.viewport.move(*self.position)
+
+    def render(self, delta_time: float) -> tuple[pygame.Surface, pygame.typing.Point]:
+        return (self.surface.subsurface(self.viewport), self.get_rect())
