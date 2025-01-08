6c9b12326d67f3ac19227f0e7776b2ffa1062eda
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-01-07 22:06:34 -0500

    Adds entity counting to EntityManager

--- src/pyrite/_data_classes/entity_manager.py
+++ src/pyrite/_data_classes/entity_manager.py
@@ -75,6 +75,12 @@ class EntityManager(ABC):
         """
         pass
 
+    # Profiling methods
+
+    @abstractmethod
+    def get_number_entities(self) -> int:
+        pass
+
     @staticmethod
     def get_entity_manager(**kwds) -> EntityManager:
         """
@@ -116,3 +122,6 @@ class DefaultEntityManager(EntityManager):
     def const_update(self, timestep: float):
         for entity in self.entities:
             entity.const_update(timestep)
+
+    def get_number_entities(self) -> int:
+        return len(self.entities)
