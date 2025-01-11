e6875949997696cd7434ecc1b30b4dd8e437e521
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-01-10 21:42:13 -0500

    Removes uneeded logging from _base_type

--- src/pyrite/types/_base_type.py
+++ src/pyrite/types/_base_type.py
@@ -1,15 +1,11 @@
 from __future__ import annotations
 
-import logging
 from typing import TYPE_CHECKING
 
 import src.pyrite._helper.defaults as defaults
 
 if TYPE_CHECKING:
     from src.pyrite.types import Container
-
-
-logger = logging.getLogger(__name__)
 
 
 class _BaseType:
@@ -29,7 +25,6 @@ class _BaseType:
         self._enabled = value
         # if self.container is None:
         if self.container is None:
-            logger.warning(f"{self} has no container for enabling/disabling.")
             return
         if value:
             self.container.enable(self)
