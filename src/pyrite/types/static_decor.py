733a5064facc1ae7d3c344d40607907f71b3db0e
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-01-13 12:59:56 -0500

    Updates docstrings

--- src/pyrite/types/static_decor.py
+++ src/pyrite/types/static_decor.py
@@ -16,6 +16,10 @@ from .renderable import Renderable
 
 
 class StaticDecor(Renderable):
+    """
+    A basic renderable with a world position and a surface to display.
+    Has no behavior, just renders.
+    """
 
     def __init__(
         self,
@@ -26,6 +30,18 @@ class StaticDecor(Renderable):
         layer: Layer = None,
         draw_index=0,
     ) -> None:
+        """
+        A Decor object with no behavior. Renders a surface based on its position.
+
+        :param display_surface: The image or surface to be shown.
+        :param position: Location of the Decor object, in world space,
+        defaults to (0, 0)
+        :param container: Container object for the renderable, defaults to None
+        :param enabled: Whether or not the object should be active immediately upon
+        spawn, defaults to True
+        :param layer: Render layer to which the object belongs, defaults to None
+        :param draw_index: Draw order for the renderable, defaults to 0
+        """
         super().__init__(container, enabled, layer, draw_index)
         self.display_surface = display_surface
         self.position = pygame.Vector2(position)
