4bb27d8ae1378c12672972dccf8ca806a95eb94c
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-01-14 12:30:56 -0500

    Adds mouse module to pyrite

---
+++ src/pyrite/mouse.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from . import game
+
+import pygame
+
+if TYPE_CHECKING:
+    pass
+
+
+def get_pos() -> tuple[int, int]:
+    mouse_screen_pos = pygame.mouse.get_pos()
+    game_instance = game.get_game_instance()
+    if game_instance is None:
+        return mouse_screen_pos
