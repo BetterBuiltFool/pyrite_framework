f6f8f76f2bc49350f44cd52bf59ce8863476e912
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-01-15 20:52:29 -0500

    Adds ChaseCamera to subpackage imports

--- src/pyrite/types/__init__.py
+++ src/pyrite/types/__init__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Protocol, runtime_checkable, TYPE_CHECKING
 
 from .surface_sector import SurfaceSector  # noqa: F401
-from .camera import CameraBase, Camera  # noqa: F401
+from .camera import CameraBase, Camera, ChaseCamera  # noqa: F401
 from .entity import Entity  # noqa: F401
 from .enums import RenderLayers, Anchor  # noqa: F401
 from .renderable import Renderable  # noqa: F401
