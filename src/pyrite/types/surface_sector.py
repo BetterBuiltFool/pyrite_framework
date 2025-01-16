ac85df4cc58acb8d8c609c1b741023150a2e47f5
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-01-15 20:41:56 -0500

    Changes screen_sector to surface_sector

--- src/pyrite/types/surface_sector.py
+++ src/pyrite/types/surface_sector.py
@@ -8,18 +8,18 @@ if typing.TYPE_CHECKING:
     from pygame.typing import RectLike
 
 
-class ScreenSector:
+class SurfaceSector:
     """
-    Represents a portion of the screen for rendering out cameras.
+    Represents a portion of a surface, primarily for rendering out cameras.
     """
 
     def __init__(self, frect: pygame.FRect | RectLike = (0, 0, 1, 1)) -> None:
         """
-        Represents a portion of the screen for rendering out cameras.
+        Represents a portion of a surface, primarily for rendering out cameras.
 
-        :param frect: A float rect representing the portion of the screen the sector
+        :param frect: A float rect representing the portion of a surface the sector
         takes up. Values should be between 0 and 1. Defaults to (0, 0, 1, 1),
-        full window.
+        full surface.
         """
         self.frect = pygame.FRect(frect)
 
