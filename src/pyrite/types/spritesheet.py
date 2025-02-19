703fae8480904b9490223cc44ee481672e93bf16
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-02-19 12:41:25 -0500

    Adds skeleton of spritesheet related classes

---
+++ src/pyrite/types/spritesheet.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from abc import ABC
+
+# import typing
+
+# import pygame
+
+
+# if typing.TYPE_CHECKING:
+# from . import Container
+# from .enums import Layer, Anchor
+# from pygame import Surface, Rect
+# from pygame.typing import Point
+
+
+from .renderable import Renderable
+
+# from .enums import AnchorPoint
+
+
+class StateDict(ABC):
+
+    pass
+
+
+class SpriteSheet(Renderable):
+
+    pass
