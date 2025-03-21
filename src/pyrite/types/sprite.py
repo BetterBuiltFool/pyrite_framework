f49f7f05def87216abab350d6bf7c24335db9573
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-03-21 13:20:07 -0400

    Updates docstrings for sprite properties.

--- src/pyrite/types/sprite.py
+++ src/pyrite/types/sprite.py
@@ -57,18 +57,40 @@ class Sprite(Renderable):
 
     @property
     def flip_x(self) -> bool:
+        """
+        Shows if the image is set to be flipped along the x axis.
+        """
         return self._flip_x
 
     @flip_x.setter
     def flip_x(self, flag: bool):
+        """
+        Sets the sprite to be flipped along the x axis.
+        Note: setting this directly will not automatically flip the display. Use
+        set_surface() for that, or call _force_update_surface() after setting the flip
+        parameters.
+
+        :param flag: Boolean determining whether to flip the sprite image.
+        """
         self._flip_x = flag
 
     @property
     def flip_y(self) -> bool:
+        """
+        Shows if the image is set to be flipped along the y axis.
+        """
         return self._flip_y
 
     @flip_y.setter
     def flip_y(self, flag: bool):
+        """
+        Sets the sprite to be flipped along the y axis.
+        Note: setting this directly will not automatically flip the display. Use
+        set_surface() for that, or call _force_update_surface() after setting the flip
+        parameters.
+
+        :param flag: Boolean determining whether to flip the sprite image.
+        """
         self.flip_y = flag
 
     def set_surface(self, sprite_image: Surface, flip_x=None, flip_y=None):
