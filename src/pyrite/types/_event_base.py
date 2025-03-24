8146cb97ca33b12b2e9a5e69ebc96bab50019fb7
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-03-24 13:36:16 -0400

    Adds InstanceEvent base class This is inherited from old pyperdoll project.

---
+++ src/pyrite/types/_event_base.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+from abc import ABC
+from collections.abc import Callable
+from typing import TypeVar
+from weakref import ref, WeakKeyDictionary
+
+
+T = TypeVar("T", bound="EventBase")
+
+
+class InstanceEvent(ABC):
+    instances: WeakKeyDictionary[T, InstanceEvent] = WeakKeyDictionary()
+
+    def __init__(self, instance: T) -> None:
+        self.instance = ref(instance)
+        """
+        Adds a reference to the owning instance, in case the event requires it.
+        """
+        self.listeners = set()
+        """
+        A set containing all listeners for this event instance.
+        TODO Make a WeakSet? Otherwise may try and call dead functions.
+        TODO Add a set for listeners that are non-static methods.
+        """
+
+    def __init_subclass__(cls) -> None:
+        # Using a WeakKeyDictionary since we only need the instance if the
+        # EventBase instance is still around. We don't need to keep hold on the
+        # EventBase instance.
+        cls.instances = WeakKeyDictionary()
+
+    def _register(self, listener: Callable):
+        self.listeners.add(listener)
+
+    def _deregister(self, listener: Callable):
+        self.listeners.discard(listener)
+
+    def _notify(self, *args, **kwds):
+        """
+        Calls all registered listeners, passing along the args and kwds.
+        This is never called directly, the instance event subclass will have its own
+        defined call method that defines its parameters, which are passed on to the
+        listeners.
+        TODO Add threading/async support options.
+        """
+        for listener in self.listeners:
+            listener(*args, **kwds)
+
+
+class EventBase(ABC):
+    def __init__(self) -> None:
+        super().__init__()
