11a0e748d917eebbe9652682274f51096502d805
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-03-26 13:09:31 -0400

    Adds kwds argument to start_thread

--- src/pyrite/utils/threading.py
+++ src/pyrite/utils/threading.py
@@ -9,21 +9,23 @@ import threading
 class BaseThreader(ABC):
 
     @abstractmethod
-    def start_thread(self, callable_: Callable, *args) -> None: ...
+    def start_thread(self, callable_: Callable, *args, **kwds) -> None: ...
 
 
 class DefaultThreader(BaseThreader):
-    def start_thread(self, callable: Callable, *args) -> None:
-        threading.Thread(target=callable, args=args).start()
+    def start_thread(self, callable: Callable, *args, **kwds) -> None:
+        threading.Thread(target=callable, args=args, kwargs=kwds).start()
 
 
 class AsyncThreader(BaseThreader):
-    def start_thread(self, callable: Callable, *args) -> None:
-        asyncio.create_task(callable(*args))
+    def start_thread(self, callable: Callable, *args, **kwds) -> None:
+        asyncio.create_task(callable(*args, **kwds))
 
 
+# Probably don't need classes for this, honestly just assigning two different functions
+# would be sufficient.
 active_threader = DefaultThreader()
 
 
-def start_thread(callable: Callable, *args) -> None:
-    active_threader.start_thread(callable, *args)
+def start_thread(callable: Callable, *args, **kwds) -> None:
+    active_threader.start_thread(callable, *args, **kwds)
