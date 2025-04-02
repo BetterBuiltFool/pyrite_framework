091e16a60d6d1618ffda7b54bf622337e2c6d0c7
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-04-02 12:46:14 -0400

    Adds properties for rotation, scale

--- src/pyrite/types/transform.py
+++ src/pyrite/types/transform.py
@@ -1,13 +1,17 @@
 from __future__ import annotations
 
 from pygame.typing import Point
-from pygame import Surface, Vector2
+from pygame import Vector2
 
 
 class Transform:
 
-    def __init__(self, position: Point) -> None:
+    def __init__(
+        self, position: Point = (0, 0), rotation: float = 0, scale: Point = (1, 1)
+    ) -> None:
         self._position = Vector2(position)
+        self._rotation = rotation
+        self._scale = Vector2(scale)
 
     @property
     def position(self) -> Vector2:
@@ -17,5 +21,18 @@ class Transform:
     def position(self, new_position: Point):
         self._position = Vector2(new_position)
 
-    def update_image(self, image: Surface) -> Surface:
-        return image
+    @property
+    def rotation(self) -> float:
+        return self._rotation
+
+    @rotation.setter
+    def rotation(self, angle: float):
+        self._rotation = angle
+
+    @property
+    def scale(self) -> Vector2:
+        return self._scale
+
+    @scale.setter
+    def scale(self, new_scale: Point):
+        self._scale = Vector2(new_scale)
