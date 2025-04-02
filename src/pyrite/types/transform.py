4d375d6507aecee5059695fdbdef63463d2066e3
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-04-02 06:46:34 -0400

    Adds position property and update_image method

--- src/pyrite/types/transform.py
+++ src/pyrite/types/transform.py
@@ -1,10 +1,21 @@
 from __future__ import annotations
 
 from pygame.typing import Point
-from pygame import Vector2
+from pygame import Surface, Vector2
 
 
 class Transform:
 
     def __init__(self, position: Point) -> None:
-        self.position = Vector2(position)
+        self._position = Vector2(position)
+
+    @property
+    def position(self) -> Vector2:
+        return self._position
+
+    @position.setter
+    def position(self, new_position: Point):
+        self._position = Vector2(new_position)
+
+    def update_image(self, image: Surface) -> Surface:
+        return image
