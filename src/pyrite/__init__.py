5f476df22148032560b92795d5e85e460b332fed
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-04-05 09:27:30 -0400

    Adds a getter for the active system manager

--- src/pyrite/__init__.py
+++ src/pyrite/__init__.py
@@ -3,10 +3,15 @@ import pygame
 from .core.display_settings import DisplaySettings
 from .core.game_data import GameData  # noqa:F401
 from .core.rate_settings import RateSettings  # noqa:F401
-from .game import Game, AsyncGame  # noqa:F401
+from .game import Game, AsyncGame, get_game_instance  # noqa:F401
 from .types.entity import Entity  # noqa:F401
 from .types.renderable import Renderable  # noqa:F401
 from .types.enums import RenderLayers, AnchorPoint  # noqa: F401
+
+
+def get_system_manager():
+    return get_game_instance().system_manager
+
 
 DEFAULT_WINDOWED: DisplaySettings = DisplaySettings()
 """
