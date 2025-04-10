da26df765d9bdc671668096b2a9b614a036a2458
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-04-10 12:58:35 -0400

    Adds a getter for the render_manager

--- src/pyrite/game.py
+++ src/pyrite/game.py
@@ -73,6 +73,18 @@ def get_entity_manager() -> EntityManager:
     return _active_instance.entity_manager
 
 
+def get_render_manager() -> RenderManager:
+    """
+    Gets the RenderManager from the current game instance.
+
+    :raises RuntimeError: If no valid game is running
+    """
+    if _active_instance is None:
+        raise RuntimeError("Cannot get system manager without a game instance running.")
+
+    return _active_instance.render_manager
+
+
 def _retrieve_system_manager() -> SystemManager:
     return _active_instance.system_manager
 
