8040994a106bd23b9dd7cd4df1a939406a788e74
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-04-10 13:01:16 -0400

    Exposes manager getters to top level imports

--- src/pyrite/__init__.py
+++ src/pyrite/__init__.py
@@ -3,7 +3,15 @@ import pygame
 from .core.display_settings import DisplaySettings
 from .core.game_data import GameData  # noqa:F401
 from .core.rate_settings import RateSettings  # noqa:F401
-from .game import Game, AsyncGame, get_system_manager, get_game_instance  # noqa:F401
+from .game import (  # noqa:F401
+    Game,
+    AsyncGame,
+    get_system_manager,
+    get_game_instance,
+    get_entity_manager,
+    get_render_manager,
+    get_renderer,
+)
 from .types.camera import Camera, ChaseCamera  # noqa:F401
 from .types.entity import Entity  # noqa:F401
 from .types.renderable import Renderable  # noqa:F401
