1ae9446d383ea1dd089a561344880ebcf94d977f
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-04-13 09:07:17 -0400

    Makes getters and setters for transform values

--- src/pyrite/transform/transform_service.py
+++ src/pyrite/transform/transform_service.py
@@ -1,1 +1,29 @@
 from __future__ import annotations
+
+from typing import TYPE_CHECKING
+from weakref import WeakKeyDictionary
+
+from .transform import Transform
+
+if TYPE_CHECKING:
+    from .transform_component import TransformComponent
+
+
+local_transforms: WeakKeyDictionary[TransformComponent, Transform] = WeakKeyDictionary()
+world_transforms: WeakKeyDictionary[TransformComponent, Transform] = WeakKeyDictionary()
+
+
+def get_local(component: TransformComponent) -> Transform:
+    return local_transforms.get(component)
+
+
+def set_local(component: TransformComponent, value: Transform):
+    world_transforms.update({component: value})
+
+
+def get_world(component: TransformComponent) -> Transform:
+    return local_transforms.get(component)
+
+
+def set_world(component: TransformComponent, value: Transform):
+    world_transforms.update({component: value})
