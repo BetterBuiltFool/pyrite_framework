4a70e99c34a56fb847dda8897e8571e8728e56d5
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-04-13 16:00:26 -0400

    Adds docstrings for local getters/setters

--- src/pyrite/transform/transform_service.py
+++ src/pyrite/transform/transform_service.py
@@ -19,36 +19,80 @@ dirty_components: WeakSet[TransformComponent] = WeakSet()
 
 
 def get_local(component: TransformComponent) -> Transform:
+    """
+    :param component: Any transform component.
+    :return: The Transform object representing _component_ in local space.
+    """
     return local_transforms.get(component)
 
 
 def get_local_position(component: TransformComponent) -> Point:
+    """
+    :param component: Any transform component.
+    :return: The current position of _component_, in local space.
+    """
     return local_transforms.get(component).position
 
 
 def get_local_rotation(component: TransformComponent) -> float:
+    """
+    :param component: Any transform component.
+    :return: The current rotation of _component_, in local space.
+    """
     return local_transforms.get(component).rotation
 
 
 def get_local_scale(component: TransformComponent) -> Point:
+    """
+    :param component: Any transform component.
+    :return: The current scaling factor of _component_, in local space.
+    """
     return local_transforms.get(component).scale
 
 
 def set_local(component: TransformComponent, value: Transform):
+    """
+    Forces the local transform value of a transform component to a new value.
+    Does not mark the component for updates.
+
+    :param component: Any transform component.
+    :param value: A Transform value, in local space.
+    """
     local_transforms.update({component: value})
 
 
 def set_local_position(component: TransformComponent, position: Point):
+    """
+    Sets the position of the transform component to the new position.
+    The component will be marked for updating.
+
+    :param component: Any transform component.
+    :param position: A point in local space.
+    """
     dirty_components.add(component)
     local_transforms.get(component).position = Vector2(position)
 
 
 def set_local_rotation(component: TransformComponent, angle: Point):
+    """
+    Sets the rotation of the transform component to the new rotation.
+    The component will be marked for updating.
+
+    :param component: Any transform component.
+    :param angle: An angle in local space.
+    """
     dirty_components.add(component)
     local_transforms.get(component).rotation = angle
 
 
 def set_local_scale(component: TransformComponent, scale: Point):
+    """
+    Sets the scale of the transform component to the new scaling factor.
+    The component will be marked for updating.
+
+    :param component: Any transform component.
+    :param position: A tuple with scaling factors for each dimension, in local space.
+    """
     dirty_components.add(component)
     local_transforms.get(component).scale = Vector2(scale)
 
