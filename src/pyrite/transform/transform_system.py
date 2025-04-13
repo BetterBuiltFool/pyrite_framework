2cb117b55faf087373c63acc46bd0ac0d6fa6001
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-04-13 10:11:24 -0400

    Adds default implementation of transform_system

--- src/pyrite/transform/transform_system.py
+++ src/pyrite/transform/transform_system.py
@@ -4,6 +4,7 @@ from abc import abstractmethod
 
 from ..types import System
 from .transform import Transform
+from . import transform_service
 
 
 class TransformSystem(System):
@@ -15,3 +16,18 @@ class TransformSystem(System):
     @abstractmethod
     def to_local(self, transform: Transform, context: Transform) -> Transform:
         pass
+
+
+class DefaultTransformSystem(TransformSystem):
+
+    def to_world(self, transform: Transform, context: Transform) -> Transform:
+        return transform
+
+    def to_local(self, transform: Transform, context: Transform) -> Transform:
+        return transform
+
+    def pre_render(self, delta_time: float) -> None:
+        for component in transform_service.get_dirty():
+            transform = transform_service.get_local(component)
+            transform_service.set_world(component, transform)
+            transform_service.clean(component)
