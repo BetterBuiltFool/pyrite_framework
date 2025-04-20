072b561521f374ce2ce004c27d2c09b1d16c3227
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-04-20 14:11:04 -0400

    Updates test cases for ellipse support function

--- tests/test_collider.py
+++ tests/test_collider.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import math
 import pathlib
 import sys
 import unittest
@@ -20,11 +21,49 @@ class TestCollider(unittest.TestCase):
 
     def test_gjk_support(self):
 
+        # Cardinal direction, no rotation
         direction = Vector2(1, 0)
 
         result = self.circle1._gjk_support_function(direction, self.transform1)
-
         self.assertEqual(result, Vector2(6, 2))
+
+        result = self.circle2._gjk_support_function(direction, self.transform2)
+        self.assertEqual(result, Vector2(9, 5))
+
+        # Orthoginal from past, no rotation
+        direction = Vector2(0, 1)
+
+        result = self.circle1._gjk_support_function(direction, self.transform1)
+        self.assertEqual(result, Vector2(1, 7))
+
+        result = self.circle2._gjk_support_function(direction, self.transform2)
+        self.assertEqual(result, Vector2(5, 9))
+
+        # Rotated direction vector, no transform rotation
+        direction = Vector2(1, 0).rotate(45)
+
+        result = self.circle1._gjk_support_function(direction, self.transform1)
+        angle = math.cos(math.radians(45))
+        expected = Vector2(
+            (angle * 5) + self.transform1.position.x,
+            (angle * 5) + self.transform1.position.y,
+        )
+        self.assertAlmostEqual(result.x, expected.x)
+        self.assertAlmostEqual(result.y, expected.y)
+
+        # Cardinal direction, rotation of transform
+        # Rotating by given amount should be equivalent to rotating vector opposite way
+        transform3 = Transform((1, 2), -45)
+        direction = Vector2(1, 0)
+
+        result = self.circle1._gjk_support_function(direction, transform3)
+        angle = math.cos(math.radians(45))
+        expected = Vector2(
+            (angle * 5) + self.transform1.position.x,
+            (angle * 5) + self.transform1.position.y,
+        )
+        self.assertAlmostEqual(result.x, expected.x)
+        self.assertAlmostEqual(result.y, expected.y)
 
 
 if __name__ == "__main__":
