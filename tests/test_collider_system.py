34232c5898e656c945df447e0dc558804e76c95f
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-04-21 06:49:07 -0400

    Adds normal test

--- tests/test_collider_system.py
+++ tests/test_collider_system.py
@@ -31,6 +31,13 @@ class TestColliserSystem(unittest.TestCase):
 
         self.assertEqual(sp1, Vector2(18, 9))
 
+    def test_normal(self):
+        point_a = Vector2(-3, -4)
+        point_b = Vector2(3, -2)
+        normal = ColliderSystem.get_normal(point_a, point_b)
+
+        self.assertEqual(normal, Vector2(-2, 6).normalize())
+
 
 if __name__ == "__main__":
 
