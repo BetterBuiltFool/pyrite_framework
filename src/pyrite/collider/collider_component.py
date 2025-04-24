ac90ecc844961ad35987cd1adefcf05bc3a2510b
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-04-24 06:43:27 -0400

    Makes collider_component handle multiple colliders

--- src/pyrite/collider/collider_component.py
+++ src/pyrite/collider/collider_component.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from collections.abc import Sequence
 from typing import TYPE_CHECKING, Any
 from weakref import WeakKeyDictionary
 
@@ -7,18 +8,38 @@ from ..types import Component
 
 if TYPE_CHECKING:
     from ..types.collider import Collider
+    from ..transform import Transform
 
 
 class ColliderComponent(Component):
     layers: WeakKeyDictionary[ColliderComponent, int] = WeakKeyDictionary()
     collision_masks: WeakKeyDictionary[ColliderComponent, int] = WeakKeyDictionary()
-    colliders: WeakKeyDictionary[ColliderComponent, Collider] = WeakKeyDictionary()
+    colliders: WeakKeyDictionary[ColliderComponent, list[Collider]] = (
+        WeakKeyDictionary()
+    )
+    transforms: WeakKeyDictionary[ColliderComponent, list[Transform]] = (
+        WeakKeyDictionary()
+    )
 
     def __init__(
-        self, owner: Any, collider: Collider, layers: int, collision_mask: int
+        self,
+        owner: Any,
+        colliders: Collider | Sequence[Collider],
+        transforms: Transform | Sequence[Transform],
+        layers: int,
+        collision_mask: int,
     ) -> None:
         super().__init__(owner)
-        self.colliders.update({self: collider})
+
+        if not isinstance(colliders, Sequence):
+            colliders = list(colliders)
+        if not isinstance(transforms, Sequence):
+            # Use the same transform for all colliders if only one is provided.
+            transforms = [transforms for _ in colliders]
+
+        self.colliders.setdefault(self, colliders)
+        self.transforms.setdefault(self, transforms)
+
         self.layers.update({self: layers})
         self.collision_masks.update({self: collision_mask})
 
