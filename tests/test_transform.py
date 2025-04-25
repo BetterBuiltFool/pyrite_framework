153d2ab4a7b15de1816c9c6a9e8d1018d31f0bbd
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-04-25 13:13:18 -0400

    Adds tests for transform mul and rmul methods

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -4,7 +4,12 @@ import sys
 import unittest
 
 sys.path.append(str(pathlib.Path.cwd()))
-from src.pyrite.transform import Transform  # noqa:E402
+from src.pyrite.transform import Transform, TransformComponent  # noqa:E402
+
+
+class Empty:
+
+    pass
 
 
 class TestTransform(unittest.TestCase):
@@ -16,6 +21,38 @@ class TestTransform(unittest.TestCase):
         local_transform = Transform((5, 0), 0, (1, 1))
 
         modified = local_transform.generalize(world_transform)
+
+        expected = Transform((10, 0), 90, (2, 2))
+
+        self.assertEqual(modified.position, expected.position)
+        self.assertEqual(modified.rotation, expected.rotation)
+        self.assertEqual(modified.scale, expected.scale)
+
+    def test_rmul(self):
+
+        world_transform = Transform((10, 10), 90, (2, 2))
+
+        local_transform = Transform((5, 0), 0, (1, 1))
+
+        modified: Transform = world_transform * local_transform
+
+        expected = Transform((10, 0), 90, (2, 2))
+
+        self.assertEqual(modified.position, expected.position)
+        self.assertEqual(modified.rotation, expected.rotation)
+        self.assertEqual(modified.scale, expected.scale)
+
+    def test_mul(self):
+
+        empty = Empty()
+
+        world_transform = TransformComponent(empty, (10, 10), 90, (2, 2))
+
+        local_transform = Transform((5, 0), 0, (1, 1))
+
+        # TransformComponent has no __mul__, so Transform.__rmul__ takes over and
+        # treats it like a transform.
+        modified: Transform = world_transform * local_transform
 
         expected = Transform((10, 0), 90, (2, 2))
 
