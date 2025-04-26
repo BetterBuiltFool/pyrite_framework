26948ea3cd89c2da10873f143cc8dde5b40be8c2
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-04-26 09:00:37 -0400

    Adds WhileTouching Event

--- src/pyrite/events/__init__.py
+++ src/pyrite/events/__init__.py
@@ -20,3 +20,18 @@ class OnTouch(InstanceEvent):
         self, this_collider: ColliderComponent, touching: ColliderComponent
     ) -> None:
         return super().__call__(this_collider, touching)
+
+
+class WhileTouching(InstanceEvent):
+    """
+    Called every frame that two collider components overlap.
+
+    :param this_collider: The collider component belonging to the owner of the instance
+        event.
+    :param touching: The collider component in contact with _this_collider_
+    """
+
+    def __call__(
+        self, this_collider: ColliderComponent, touching: ColliderComponent
+    ) -> None:
+        return super().__call__(this_collider, touching)
