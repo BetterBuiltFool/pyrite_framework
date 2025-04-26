f3280e860a255cc28d31c9ca2945281960d7e609
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-04-26 08:44:04 -0400

    Adds OnTouch event

--- src/pyrite/events/__init__.py
+++ src/pyrite/events/__init__.py
@@ -1,3 +1,22 @@
+from typing import TYPE_CHECKING
 from ..types.instance_event import InstanceEvent  # noqa:F401
 
+if TYPE_CHECKING:
+    from ..collider.collider_component import ColliderComponent
+
 # Define common events here.
+
+
+class OnTouch(InstanceEvent):
+    """
+    Called whenever a collider begins contact with another.
+
+    :param this_collider: The collider component belonging to the owner of the instance
+        event.
+    :param touching: The collider component in contact with _this_collider_
+    """
+
+    def __call__(
+        self, this_collider: ColliderComponent, touching: ColliderComponent
+    ) -> None:
+        return super().__call__(this_collider, touching)
