fab040a1999c75d5cd39f6202d1fa082bcc24545
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-04-30 06:42:14 -0400

    Adds package level imports to shapes

---
+++ src/pyrite/shapes/__init__.py
@@ -0,0 +1,9 @@
+from typing import TypeAlias
+
+from .ellipse import Ellipse  # noqa:F401
+from .point import Point  # noqa:F401
+from .polygon import Polygon  # noqa:F401
+from .rectangle import Rectangle  # noqa:F401
+from .stadium import Stadium
+
+Pill: TypeAlias = Stadium
