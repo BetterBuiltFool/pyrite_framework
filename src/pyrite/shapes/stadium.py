630f437d0410c53b79aba21f8941f05e7b69ee30
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-04-30 06:39:02 -0400

    Adds Stadium shape

---
+++ src/pyrite/shapes/stadium.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import math
+
+from pygame import Vector2
+
+from ..transform import Transform
+from ..types.shape import Shape
+
+
+_vertical_axis = Vector2(0, 1)
+
+
+class Stadium(Shape):
+
+    def __init__(self, radius: float, height: float) -> None:
+        self.radius = radius
+        self.height = height
+
+    def get_furthest_vertex(self, vector: Vector2, transform: Transform) -> Vector2:
+        # Rotate and scale vector by transform data
+        # normalize vector
+        vector = self._prescale_vector(vector, transform)
+        # Find the farthest point of the shape (In this case, it's now a circle)
+        origin = vector * _vertical_axis
+        y_offset = math.copysign(self.height / 2, origin)
+        point = Vector2(0, y_offset) + (self.radius * vector)
+        # Add the position component of transform
+        point = point.elementwise() * transform.scale
+        point = point.rotate(transform.rotation)
+        point += transform.position
+        # return position
+        return point
