4de663032ece7f0818ec5dc63eba772e01eb913e
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-01 22:28:36 -0400

    Updates docstrings for Polygon

--- src/pyrite/shapes/polygon.py
+++ src/pyrite/shapes/polygon.py
@@ -18,8 +18,23 @@ def _get_furthest(direction: Vector2, point_a: Vector2, point_b: Vector2) -> Vec
 
 
 class Polygon(Shape):
+    """
+    A shape made from arbitray vertices.
+    Must be convex to draw or collide properly.
+
+    :Vertices: Custom.
+    """
 
     def __init__(self, vertices: Sequence[Vector2]) -> None:
+        """
+        A shape made from arbitray vertices.
+        Must be convex to draw or collide properly.
+
+        If the vertices are not in order, the collsion algorithm will operate on the
+        convex hull of the shape.
+
+        :param vertices: A sequence of points representing the vertices of the polygon.
+        """
         self._vertices = list(vertices)
 
     def get_furthest_vertex(self, vector: Vector2, transform: Transform) -> Vector2:
@@ -49,6 +64,8 @@ class Polygon(Shape):
         fill_color: ColorLike = None,
     ) -> Surface:
         surface = super().draw(edge_width, edge_color, fill_color)
+        # TODO Implement this!
+        raise NotImplementedError("Cannot draw polygons yet.")
         if fill_color is not None:
             # Draw the filled polygon by forming triangles from edges to origin
             pass
