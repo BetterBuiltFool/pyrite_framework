91f33211e2b01aff0acb17f65aee5ecea4e06ee6
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-11 19:22:44 -0400

    Fixes testing issues in RigidbodyComponent

--- src/pyrite/physics/rigidbody.py
+++ src/pyrite/physics/rigidbody.py
@@ -4,13 +4,14 @@ from typing import Any, TYPE_CHECKING
 from weakref import WeakValueDictionary
 
 from ..types import Component
+from .physics_service import PhysicsService
 
 if TYPE_CHECKING:
     from pymunk import Body
 
 
 class RigidbodyComponent(Component):
-    _bodies: WeakValueDictionary[Body, Any] = WeakValueDictionary
+    _bodies: WeakValueDictionary[Body, Any] = WeakValueDictionary()
     """
     Associates an owner object with a physics body
     """
@@ -18,7 +19,8 @@ class RigidbodyComponent(Component):
     def __init__(self, owner: Any, body: Body) -> None:
         super().__init__(owner)
         self.body = body
-        self._bodies.update({body, owner})
+        self._bodies.update({body: owner})
+        PhysicsService.space.add(body)
 
     @classmethod
     def get_owner_from_body(cls, body: Body) -> Any | None:
