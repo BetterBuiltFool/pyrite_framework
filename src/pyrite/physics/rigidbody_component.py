5f9a12f00838cb9e00c2e051fb1a5da9b11dafd9
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-12 13:29:56 -0400

    Makes TransformComponent a requirement for RBComp

--- src/pyrite/physics/rigidbody_component.py
+++ src/pyrite/physics/rigidbody_component.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import Any, TYPE_CHECKING
 from weakref import WeakValueDictionary
 
+from ..transform import TransformComponent
 from ..types import Component
 from .physics_service import PhysicsService
 
@@ -18,6 +19,11 @@ class RigidbodyComponent(Component):
 
     def __init__(self, owner: Any, body: Body) -> None:
         super().__init__(owner)
+        if not (transform := TransformComponent.get(owner)):
+            raise RuntimeError(
+                f"RigidbodyComponent requires that {owner} has a TransformComponent"
+            )
+        self.transform = transform
         self.body = body
         self._bodies.update({body: owner})
         PhysicsService.space.add(body)
