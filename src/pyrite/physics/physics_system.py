08dfe70e627539a2205bce9722b30e22cc8662b1
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-12 13:26:29 -0400

    Attempts to interpolate transform to body

--- src/pyrite/physics/physics_system.py
+++ src/pyrite/physics/physics_system.py
@@ -3,10 +3,12 @@ from __future__ import annotations
 import math
 from typing import TYPE_CHECKING
 
+import pymunk
+
 from ..types import System
 from .physics_service import PhysicsService
 from .rigidbody_component import RigidbodyComponent
-from ..transform import TransformComponent
+from ..transform import TransformComponent, transform_service
 
 if TYPE_CHECKING:
     pass
@@ -20,6 +22,9 @@ class PhysicsSystem(System):
 
         PhysicsService.step(timestep)
 
+        self.sync_transforms_to_bodies()
+
+    def update(self, delta_time: float) -> None:
         self.sync_transforms_to_bodies()
 
     def sync_bodies_to_transforms(self):
@@ -46,7 +51,23 @@ class PhysicsSystem(System):
         rotation with the new calculations.
         """
         for body, key_object in RigidbodyComponent._bodies.items():
-            if not (transform := TransformComponent.get(key_object)):
+            if (
+                not (transform := TransformComponent.get(key_object))
+                or body.is_sleeping
+                or body.body_type == pymunk.Body.STATIC
+            ):
+                # No adjustments to sleeping, static, or transformless objects
                 continue
-            transform.world_position = body.position
-            transform.world_rotation = math.degrees(body.angle)
+            new_pos = transform.world_position.lerp(body.position, 0.1)
+            angle_between = (
+                (math.degrees(body.angle) - transform.world_rotation) + 180
+            ) % 360 - 180
+            new_rot = angle_between / 2
+
+            print(f"{key_object} {new_pos=}, {transform.world_position=}")
+
+            transform_service.set_world_position(transform, new_pos)
+            transform_service.set_world_rotation(transform, new_rot)
+
+            # transform.world_position = body.position
+            # transform.world_rotation = math.degrees(body.angle)
