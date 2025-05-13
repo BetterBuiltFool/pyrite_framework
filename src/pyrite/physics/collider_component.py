fb22e231c5e8016b6d43a939e22b8165aa2d8937
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-12 20:10:49 -0400

    Adds collision events to collider_component

--- src/pyrite/physics/collider_component.py
+++ src/pyrite/physics/collider_component.py
@@ -6,9 +6,10 @@ from weakref import WeakKeyDictionary
 
 from pymunk import ShapeFilter
 
-from ..types import Component
-from .rigidbody_component import RigidbodyComponent
+from ..events import OnSeparate, OnTouch, WhileTouching
 from .physics_service import PhysicsService, COMPONENT_TYPE
+from .rigidbody_component import RigidbodyComponent
+from ..types import Component
 
 if TYPE_CHECKING:
     from pymunk import Shape
@@ -46,6 +47,48 @@ class ColliderComponent(Component):
             collision_shape.body = body
             collision_shape.filter = self.filter
             PhysicsService.space.add(collision_shape)
+
+        self.OnTouch = OnTouch(self)
+        """
+        Called whenever a collider begins contact with another.
+
+        :Signature:
+        on_touch(this_collider: ColliderComponent, touching: ColliderComponent) -> None
+
+
+        this_collider: The collider component belonging to the owner of the
+            instance event.
+        touching: The collider component in contact with _this_collider_
+        """
+
+        self.WhileTouching = WhileTouching(self)
+        """
+        Called every frame that two collider components overlap.
+
+        :Signature:
+        on_touch(this_collider: ColliderComponent, touching: ColliderComponent) -> None
+
+
+        this_collider: The collider component belonging to the owner of the
+            instance event.
+        touching: The collider component in contact with _this_collider_
+        """
+
+        self.OnSeparate = OnSeparate(self)
+        """
+        Called whenever a previously touching collider stops touching.
+
+        :Signature:
+        on_separate(
+            this_collider: ColliderComponent,
+            touching: ColliderComponent
+        ) -> None
+
+
+        this_collider: The collider component belonging to the owner of the
+            instance event.
+        touching: The collider component formerly in contact with _this_collider_
+        """
 
     @property
     def category(self) -> int:
