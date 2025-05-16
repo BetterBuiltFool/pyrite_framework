09bc1dcdb83cdd84e4d53208de25e2cb91bd0250
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-15 22:02:32 -0400

    Add default body generation to rigidbody_component

--- src/pyrite/physics/rigidbody_component.py
+++ src/pyrite/physics/rigidbody_component.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
 from typing import Any, TYPE_CHECKING
+
+from pymunk import Body
+
 from ..transform import TransformComponent
 from ..types import Component
 from .physics_service import PhysicsService
 
 if TYPE_CHECKING:
-    from pymunk import Body
+    pass
 
 
 class RigidbodyComponent(Component):
@@ -14,12 +17,14 @@ class RigidbodyComponent(Component):
     Associates an owner object with a physics body
     """
 
-    def __init__(self, owner: Any, body: Body) -> None:
+    def __init__(self, owner: Any, body: Body = None) -> None:
         super().__init__(owner)
         if not (transform := TransformComponent.get(owner)):
             raise RuntimeError(
                 f"RigidbodyComponent requires that {owner} has a TransformComponent"
             )
+        if body is None:
+            body = Body()
         self.transform = transform
         self.body = body
         PhysicsService.bodies.update({body: owner})
