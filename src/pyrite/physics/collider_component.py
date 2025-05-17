1a5d3c3cf029e6ec3be0bbae59057cec888ab06e
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-17 10:39:03 -0400

    Updates docstrings for collider_component

--- src/pyrite/physics/collider_component.py
+++ src/pyrite/physics/collider_component.py
@@ -16,6 +16,22 @@ if TYPE_CHECKING:
 
 
 class ColliderComponent(Component):
+    """
+    Component that manages collision shapes for a RigidbodyComponent.
+
+    Has cooresponding events for collisions with other ColliderComponents.
+
+    Uses bitmasks for controlling collisions.
+
+    Requires RigidbodyComponent.
+
+    ### Events:
+    - OnTouch: Called on first contact with another ColliderComponent.
+    - OnSeparate: Called when the ColliderComponent breaks contact with another.
+    - WhileTouching: Called _every_ physics tick while contacting another
+        ColliderComponent.
+    """
+
     _categories: WeakKeyDictionary[ColliderComponent, int] = WeakKeyDictionary()
     _collision_masks: WeakKeyDictionary[ColliderComponent, int] = WeakKeyDictionary()
 
@@ -92,25 +108,54 @@ class ColliderComponent(Component):
 
     @property
     def category(self) -> int:
+        """
+        A bitmask showing the collision categories to which this collider belongs.
+        Other colliders with any overlap in their collision mask will generate
+        collision events.
+        """
         return self._categories[self]
 
     def add_categories_layer(self, layer: int):
+        """
+        Takes a bitmask value and ensure that the Collider's category includes it.
+        """
         self._categories[self] |= layer
 
     def remove_categories_layer(self, layer: int):
+        """
+        Takes a bitmask value and ensures that the Collider's category excludes it.
+        """
         self._categories[self] &= ~layer
 
     @property
     def collision_mask(self) -> int:
+        """
+        A bitmask showing the collision layers with which this collider will generate
+        events.
+        """
         return self._collision_masks[self]
 
     def add_collision_mask_layer(self, layer: int):
+        """
+        Takes a bitmask value and ensure that the Collider's collision mask includes it.
+        """
         self._collision_masks[self] |= layer
 
     def remove_collision_mask_layer(self, layer: int):
+        """
+        Takes a bitmask value and ensures that the Collider's collision mask excludes
+        it.
+        """
         self._collision_masks[self] &= ~layer
 
     def compare_mask(self, other: ColliderComponent) -> bool:
+        """
+        Compares the collision mask with the category of the other component to
+        determine if they overlap.
+
+        :param other: Another ColliderComponent.
+        :return: True if the masks are overlapping, otherwise False.
+        """
         return self.collision_mask & other.category
 
     def _set_shape_filter(self):
