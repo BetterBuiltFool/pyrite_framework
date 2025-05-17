d99e1bf88e0a7df46be50e53cfe02cffdd70f5e2
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-17 09:52:59 -0400

    Removes first attempt collider stuff

--- src/pyrite/shapes/__init__.py
+++
@@ -1,9 +0,0 @@
-from typing import TypeAlias
-
-from .ellipse import Ellipse  # noqa:F401
-from .point import Point  # noqa:F401
-from .polygon import Polygon  # noqa:F401
-from .rectangle import Rectangle  # noqa:F401
-from .stadium import Stadium
-
-Pill: TypeAlias = Stadium
