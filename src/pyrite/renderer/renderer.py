dd51885c47f9e03da196e71d3a54004387c60c7c
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-18 15:00:06 -0400

    Adds sprite cleanup to sprite renderer

--- src/pyrite/renderer/renderer.py
+++ src/pyrite/renderer/renderer.py
@@ -16,6 +16,7 @@ class SpriteRenderer:
     def render(cls, sprite: Sprite, camera: CameraBase):
         if sprite.is_dirty or sprite not in cls._sprite_cache:
             # Update the cache. This will save us redraws when the sprite is unchanged.
+            sprite.is_dirty = False
             cls._sprite_cache[sprite] = sprite.draw_sprite()
         surface = cls._sprite_cache[sprite]
         camera.draw_to_view(surface, sprite.get_rect().topleft)
