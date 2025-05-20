5cb4afdedffb3a83ec1b74d63d20c28c622ff97b
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-20 19:44:47 -0400

    Adds Frustum ABC for future cameras

---
+++ src/pyrite/types/frustum.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    pass
+
+
+class Frustum(ABC):
+    """
+    An object representing the viewing area of a camera.
+    """
+
+    @abstractmethod
+    def contains(self, bounds) -> bool:
+        """
+        Determines if the bounds are contained within the Frustum.
+
+        :param bounds: A Bounds object that may or may not be within the Frustum
+        :return: True if the frustum contains the bounds, otherwise False
+        """
+        pass
