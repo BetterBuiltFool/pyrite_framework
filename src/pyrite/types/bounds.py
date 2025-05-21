c3d0de632c5e4bfd7196062e3d2b2f55fd3406db
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-21 08:43:11 -0400

    Fleshes out CullingBounds definitions

--- src/pyrite/types/bounds.py
+++ src/pyrite/types/bounds.py
@@ -4,15 +4,28 @@ from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from pygame.typing import Point
+    from pygame import Rect
+    from ..cuboid import Cuboid
 
 
 class CullingBounds(ABC):
 
     @abstractmethod
-    def get_volume(self) -> list[Point]:
+    def get_volume(self) -> Cuboid:
+        """
+        Generates a volume representing the location of the bounds in 3D space.
+
+        :return: A Cuboid that describes the bounds in 3D space.
+            2D objects are represented by depth = 0.
+        """
         pass
 
     @abstractmethod
-    def flatten(self) -> list[Point]:
+    def flatten(self) -> Rect:
+        """
+        Generates an area representing the bounds in 2D space.
+
+        :return: A Rect that describes the bounds in 2D space. 3D objects are projected
+            onto a plane before the rect in calculated.
+        """
         pass
