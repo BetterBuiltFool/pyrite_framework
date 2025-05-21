56ae8a583b22f8839377e910e4f6f9cb015348cd
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-21 08:58:59 -0400

    Adds RectBounds class

---
+++ src/pyrite/rendering/rect_bounds.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+
+from ..types.bounds import CullingBounds
+from ..cuboid import Cuboid
+
+if TYPE_CHECKING:
+    from pygame import Rect
+
+
+class RectBounds(CullingBounds):
+    """
+    Bounds described by an axis-aligned rectangle on the xy-plane.
+    """
+
+    def __init__(self, rect: Rect) -> None:
+        self.rect = rect
+
+    def get_volume(self) -> Cuboid:
+        rect = self.rect
+        return Cuboid(rect.left, rect.top, 0, rect.width, rect.height, 0)
+
+    def flatten(self) -> Rect:
+        return self.rect
