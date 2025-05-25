5723d0cd85edee58e4654bee569fe79173e980db
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-25 13:22:29 -0400

    Adds DebugRenderer ABC

---
+++ src/pyrite/types/debug_renderer.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    pass
+
+
+class DebugRenderer(ABC):
+
+    @abstractmethod
+    def render(self, *args, **kwds):
+        pass
