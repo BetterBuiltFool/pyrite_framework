7bc7314960fa56f4099b5491ae4192ff874ac52c
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-25 16:37:05 -0400

    Gives debug renderer hooks instead of abstracts

--- src/pyrite/types/debug_renderer.py
+++ src/pyrite/types/debug_renderer.py
@@ -1,18 +1,19 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
+from abc import ABC
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    pass
+    from pygame import Surface
+    from ..core.render_system import RenderQueue
 
 
 class DebugRenderer(ABC):
 
-    @abstractmethod
-    def render(self, *args, **kwds):
+    def draw_to_screen(self, window: Surface, render_queue: RenderQueue):
         """
-        Generic render function.
-        Passed parameters vary depending on where the renderer gets called.
+        Gives the option to draw directly to the screen.
+
+        :param window: _description_
+        :param render_queue: _description_
         """
-        pass
