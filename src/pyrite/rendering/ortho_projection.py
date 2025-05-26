919648b86a944b763e885523a01784270fb58c7a
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-26 19:42:56 -0400

    Adds OrthoProjection for use

---
+++ src/pyrite/rendering/ortho_projection.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pygame
+from pygame import Vector3
+
+from ..types.projection import Projection
+
+if TYPE_CHECKING:
+    from pygame import Rect
+    from pygame.typing import Point
+
+
+class OrthProjection(Projection):
+
+    def __init__(
+        self, projection_rect: Rect = None, z_near: float = -1, z_far: float = 1
+    ) -> None:
+        if projection_rect is None:
+            # Default projection is the size of the screen
+            display = pygame.display.get_surface()
+            projection_rect = Rect(left_top=(0, 0), width_height=display.size)
+            projection_rect.center = (0, 0)
+        self.projection_rect = projection_rect
+        self.z_near = z_near
+        self.z_far = z_far
+
+    @property
+    def far_plane(self) -> Rect:
+        return self.projection_rect
+
+    def screen_to_NDC(self, screen_point: Point, viewport: Rect) -> Point:
+        # Viewport may be smaller or larger than the projection, so adjust.
+        width_ratio = self.projection_rect.width / viewport.width
+        height_ratio = self.projection_rect.height / viewport.height
+
+        # Convert screen space to projection space.
+        point = (
+            (screen_point[0] * width_ratio) - (self.projection_rect.width / 2),
+            (screen_point[1] * height_ratio) - (self.projection_rect.height / 2),
+        )
+        # Normalize coordinates
+        ndc_point = (
+            point[0] / (self.projection_rect.width / 2),
+            -point[1] / (self.projection_rect.height / 2),
+            self.z_far,
+        )
+        return Vector3(*ndc_point)
