1713189d1ff1d463f4a2d0aa105b70fe9890f41a
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-26 11:05:46 -0400

    Makes Surfacesector use NDC space

--- src/pyrite/camera/surface_sector.py
+++ src/pyrite/camera/surface_sector.py
@@ -3,9 +3,11 @@ from __future__ import annotations
 import typing
 
 import pygame
+from pygame import FRect, Rect
 
 if typing.TYPE_CHECKING:
     from pygame.typing import RectLike
+    from pygame import Surface
 
 
 class SurfaceSector:
@@ -13,17 +15,26 @@ class SurfaceSector:
     Represents a portion of a surface, primarily for rendering out cameras.
     """
 
-    def __init__(self, frect: pygame.FRect | RectLike = (0, 0, 1, 1)) -> None:
+    def __init__(self, frect: FRect | RectLike = None, **kwds) -> None:
         """
         Represents a portion of a surface, primarily for rendering out cameras.
+        Uses Normalized Device Coordinates (NDC), so
+        left = -1, right = 1, top = 1, bottom = -1
 
-        :param frect: A float rect representing the portion of a surface the sector
-        takes up. Values should be between 0 and 1. Defaults to (0, 0, 1, 1),
-        full surface.
+        :param frect: An FRect representing the screen sector in NDC space.
         """
-        self.frect = pygame.FRect(frect)
+        if frect is None:
+            if not (topleft := kwds.get("topleft")):
+                topleft = (-1, 1)
+            if not (bottomright := kwds.get("bottomright")):
+                bottomright = (1, -1)
+            frect = FRect()
+            frect.topleft = topleft
+            size = (bottomright[0] - topleft[0], topleft[1] - bottomright[1])
+            frect.size = size
+        self.frect = FRect(frect)
 
-    def get_display_rect(self) -> pygame.Rect:
+    def get_display_rect(self) -> Rect:
         """
         Calculates the subrect for the sector, from the display.
 
@@ -32,7 +43,7 @@ class SurfaceSector:
         """
         return self.get_rect(pygame.display.get_surface())
 
-    def get_rect(self, surface: pygame.Surface) -> pygame.Rect:
+    def get_rect(self, surface: Surface) -> Rect:
         """
         Calculates the subrect for the sector
 
@@ -41,15 +52,17 @@ class SurfaceSector:
         screen sectors' frect.
         """
         frect = self.frect
-        surface_width, surface_height = surface.get_rect().size
+        surface_rect = surface.get_rect()
+        surface_width, surface_height = surface_rect.size
+        center_x, center_y = surface_rect.center
         topleft = (
-            int(frect.left * surface_width),
-            int(frect.top * surface_height),
+            center_x - int(frect.left * (-surface_width / 2)),
+            center_y - int(frect.top * (surface_height / 2)),
         )
         size = (
-            int(frect.width * surface_width),
-            int(frect.height * surface_height),
+            int(frect.width * (surface_width / 2)),
+            int(frect.height * (surface_height / 2)),
         )
-        rect = pygame.Rect()
+        rect = Rect()
         rect.topleft, rect.size = topleft, size
         return rect
