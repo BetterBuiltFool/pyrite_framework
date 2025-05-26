092d06e0cfa05c67ec24db0211a2683324649630
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-26 14:13:12 -0400

    Adds zoom methods to CameraService

--- src/pyrite/camera/camera_service.py
+++ src/pyrite/camera/camera_service.py
@@ -29,3 +29,17 @@ class CameraService:
     def draw_to_camera(cls, camera: Camera, image: Surface, position: Point):
         surface = cls._surfaces.get(camera)
         surface.blit(image, camera.to_local(position))
+
+    @classmethod
+    def zoom(cls, camera: Camera, zoom: Point):
+        display_size = pygame.display.get_surface().size
+        surface_size = (display_size[0] * zoom[0]), (display_size[1] * zoom[1])
+        surface = Surface(surface_size)
+        cls._surfaces.update({camera: surface})
+
+    @classmethod
+    def zoom_to(cls, camera: Camera, size: Point):
+        display_size = pygame.display.get_surface().size
+        zoom_x = display_size[0] / size[0]
+        zoom_y = display_size[1] / size[1]
+        camera.zoom = (zoom_x, zoom_y)
