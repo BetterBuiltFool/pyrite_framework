e6f7275338cf9d6eb30b10d7b798ac9902de2c7a
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-26 13:59:54 -0400

    Adds add_camera to CameraService

--- src/pyrite/camera/camera_service.py
+++ src/pyrite/camera/camera_service.py
@@ -3,15 +3,28 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 from weakref import WeakKeyDictionary
 
+import pygame
+from pygame import Surface, Vector2
+
 if TYPE_CHECKING:
     from . import Camera
-    from pygame import Surface
     from pygame.typing import Point
 
 
 class CameraService:
 
     _surfaces: WeakKeyDictionary[Camera, Surface] = WeakKeyDictionary()
+
+    @classmethod
+    def add_camera(
+        cls, camera: Camera, camera_surface: Surface = None, max_size: Point = None
+    ):
+        if not camera_surface:
+            if not max_size:
+                max_size = pygame.display.get_surface().size
+            max_size = Vector2(max_size)
+            camera_surface = Surface(max_size)
+        cls._surfaces.update({camera: camera_surface})
 
     @classmethod
     def draw_to_camera(cls, camera: Camera, image: Surface, position: Point):
