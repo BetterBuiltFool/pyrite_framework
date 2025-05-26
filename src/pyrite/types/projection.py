6dd4e2997a2bc4976e0501fd9be146388609b734
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-26 19:35:44 -0400

    Updates abstract screen_to_NDC method

--- src/pyrite/types/projection.py
+++ src/pyrite/types/projection.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from pygame import FRect, Rect
+    from pygame import Rect, Vector3
     from pygame.typing import Point
 
 
@@ -18,5 +18,13 @@ class Projection(ABC):
         """
 
     @abstractmethod
-    def screen_to_NDC(self, screen_point: Point, viewport: FRect) -> Point:
+    def screen_to_NDC(self, screen_point: Point, viewport: Rect) -> Vector3:
+        """
+        Takes a point on the screen and converts it into NDC space.
+        The NDC point is along the back plane of NDC space, for allowing ray casting.
+
+        :param screen_point: A point in screen space.
+        :param viewport: A Rect describing the viewport the screen point is in.
+        :return: A 3D point in standard NDC space.
+        """
         pass
