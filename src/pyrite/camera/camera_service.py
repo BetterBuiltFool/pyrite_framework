1b50bf01aa5639e378f203fab3d69ff95a0c29de
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-05-26 20:24:31 -0400

    Adds outlines for new methods for CameraService

--- src/pyrite/camera/camera_service.py
+++ src/pyrite/camera/camera_service.py
@@ -7,6 +7,8 @@ from pygame import Surface
 
 if TYPE_CHECKING:
     from . import Camera
+    from ..types import CameraViewBounds, CullingBounds
+    from pygame import Rect
     from pygame.typing import Point
 
 
@@ -25,9 +27,90 @@ class CameraService:
         cls._surfaces.update({camera: camera_surface})
 
     @classmethod
+    def clear(cls, camera: Camera):
+        surface = cls._surfaces.get(camera)
+        surface.fill((0, 0, 0))
+
+    @classmethod
     def draw_to_camera(cls, camera: Camera, image: Surface, position: Point):
         surface = cls._surfaces.get(camera)
         surface.blit(image, camera.to_local(position))
+
+    @classmethod
+    def get_bounds(cls, camera: Camera) -> CullingBounds:
+        pass
+
+    @classmethod
+    def get_rect(cls, camera: Camera) -> Rect:
+        surface = cls._surfaces.get(camera)
+        return surface.get_rect()
+
+    @classmethod
+    def get_view_bounds(cls, camera: Camera) -> CameraViewBounds:
+        pass
+
+    @classmethod
+    def to_local(cls, camera: Camera, point: Point) -> Point:
+        # TODO Slog through this and make it work
+        # It renders correctly, but bounds end up mirrored above the sprites.
+        # point = Vector2(point)
+
+        # point -= Vector2(self.get_surface_rect().bottomleft)
+
+        # point.y = -point.y
+
+        # return point
+
+        # return point - Vector2(self.get_surface_rect().topleft)
+        pass
+
+    @classmethod
+    def to_world(cls, camera: Camera, point: Point) -> Point:
+        # point = Vector2(point)
+
+        # point += Vector2(self.get_surface_rect().bottomleft)
+
+        # point.y = -point.y
+
+        # return point
+
+        # return point + Vector2(self.get_surface_rect().topleft)
+        pass
+
+    @classmethod
+    def screen_to_world(
+        cls, camera: Camera, point: Point, sector_index: int = 0
+    ) -> Point:
+        # sector = self.surface_sectors[sector_index]
+        # sector_rect = self._get_sector_rect(sector)
+
+        # viewport_world = self.get_viewport_rect()
+
+        # viewport_space_position = self._screen_to_viewport(
+        #     point, sector_rect, Vector2(viewport_world.size)
+        # )
+
+        # return viewport_space_position.elementwise() + viewport_world.topleft
+        pass
+
+    @classmethod
+    def screen_to_world_clamped(
+        cls, camera: Camera, point: Point, sector_index: int = 0
+    ) -> Point | None:
+        # sector = self.surface_sectors[sector_index]
+        # sector_rect = self._get_sector_rect(sector)
+
+        # if not sector_rect.collidepoint(point):
+        #     return None
+
+        # viewport_world = self.get_viewport_rect()
+
+        # viewport_space_position = self._screen_to_viewport(
+        #     point, sector_rect, Vector2(viewport_world.size)
+        # )
+
+        # return viewport_space_position.elementwise() + viewport_world.topleft
+        pass
 
     @classmethod
     def zoom(cls, camera: Camera, zoom: Point):
