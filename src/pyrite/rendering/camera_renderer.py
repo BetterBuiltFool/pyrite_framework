3ebfc47bb6f6c27385788ccb44d6d0291923f883
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-06 13:21:09 -0400

    Replaces old camera render with new render

--- src/pyrite/rendering/camera_renderer.py
+++ src/pyrite/rendering/camera_renderer.py
@@ -2,13 +2,11 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import pygame
 
 from ..camera.camera_service import CameraService
 from ..types import Renderer
 
 if TYPE_CHECKING:
-    from .viewport import Viewport
     from ..camera import Camera
     from ..types.render_target import RenderTarget
 
@@ -16,16 +14,7 @@ if TYPE_CHECKING:
 class CameraRenderer(Renderer):
 
     @classmethod
-    def render(cls, camera: Camera, viewport: Viewport):
-        window = pygame.display.get_surface()
-        render_rect = viewport.get_display_rect()
-        window.blit(
-            camera.scale_view(CameraService._surfaces.get(camera), render_rect.size),
-            render_rect.topleft,
-        )
-
-    @classmethod
-    def new_render(cls, camera: Camera, render_target: RenderTarget):
+    def render(cls, camera: Camera, render_target: RenderTarget):
         surface = render_target.get_target_surface()
         render_rect = render_target.get_target_rect()
         camera_view = CameraService._surfaces.get(camera)
