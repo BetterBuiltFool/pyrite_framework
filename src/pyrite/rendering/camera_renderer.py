a8525d498aee9842c8fd1f4b30a38937e3169c20
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-06 21:33:16 -0400

    Simplifies cropping/scaling logic for camera

--- src/pyrite/rendering/camera_renderer.py
+++ src/pyrite/rendering/camera_renderer.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-
 from ..camera.camera_service import CameraService
 from ..types import Renderer
 
@@ -18,22 +17,12 @@ class CameraRenderer(Renderer):
         surface = render_target.get_target_surface()
         render_rect = render_target.get_target_rect()
         camera_view = CameraService._surfaces.get(camera)
-        if render_target.crop:
-            # If we need to crop:
-            subsurface_rect = render_rect.copy()
-            if not (
-                subsurface_rect.size[0] > camera_view.size[0]
-                or subsurface_rect.size[1] > camera_view.size[1]
-            ):
-                # We can only crop if the target is smaller.
-                # Otherwise it's an illegal operation.
-                subsurface_rect.center = camera_view.get_rect().center
-                # Crop from the center of the view
-                camera_view = camera_view.subsurface(subsurface_rect).copy()
-        else:
+        if not render_target.crop:
             # Not cropping, so scale the view instead.
             camera_view = camera.scale_view(camera_view, render_rect.size)
+        # TODO Center the view within render_rect
         surface.blit(
             camera_view,
             render_rect.topleft,
+            render_rect,
         )
