b3e4a6b687f0efcf54cd2d0f9f8dc59e0e7b10cf
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-10 06:45:01 -0400

    Makes BoudnsRenderer use camera draw_rect

--- src/pyrite/debug/bounds_renderer.py
+++ src/pyrite/debug/bounds_renderer.py
@@ -2,16 +2,17 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import pygame
 from pygame import Color
 
 from ..types.debug_renderer import DebugRenderer
 from ..enum import RenderLayers
+from ..rendering.camera_renderer import CameraRenderer
+from ..rendering.viewport import Viewport
 
 if TYPE_CHECKING:
-    from pygame import Surface
     from pygame.typing import ColorLike
     from ..core.render_system import RenderQueue
+    from ..camera import Camera
 
 
 class BoundsRenderer(DebugRenderer):
@@ -19,12 +20,17 @@ class BoundsRenderer(DebugRenderer):
     def __init__(self, draw_color: ColorLike) -> None:
         self.color = Color(draw_color)
 
-    def draw_to_screen(self, window: Surface, render_queue: RenderQueue):
+    def draw_to_screen(self, cameras: Camera, render_queue: RenderQueue):
         for layer in RenderLayers._layers:
             layer_dict = render_queue.get(layer, {})
             for renderables in layer_dict.values():
                 for renderable in renderables:
-                    bounds = renderable.get_bounds()
-                    # Draw a rect directly to screen
-                    # TODO Check against cameras to get screen pos.
-                    pygame.draw.rect(window, self.color, bounds.get_rect(), width=1)
+                    for camera in cameras:
+                        bounds = renderable.get_bounds()
+                        CameraRenderer.draw_rect(
+                            camera,
+                            Viewport.DEFAULT,
+                            self.color,
+                            bounds.get_rect(),
+                            width=1,
+                        )
