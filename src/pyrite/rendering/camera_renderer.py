a3f10ca7c17328401dc2890a526c4bc6222cb50f
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-12 12:38:49 -0400

    Movesv draw_line to match pygame draw order

--- src/pyrite/rendering/camera_renderer.py
+++ src/pyrite/rendering/camera_renderer.py
@@ -21,35 +21,6 @@ class CameraRenderer(Renderer):
     _smooth: bool = False
 
     @classmethod
-    def draw_line(
-        cls,
-        camera: Camera,
-        viewport: Viewport,
-        color: ColorLike,
-        start_pos: Point,
-        end_pos: Point,
-        width: int = 1,
-    ) -> Rect:
-        """
-        Draws a line to the screen, using the coordinates (in world space),
-        transformed to screen space.
-
-        :param camera: The camera whose view is being considered
-        :param viewport: The viewport being drawn in.
-        :param color: A color-like value to give to the line
-        :param start_pos: Start position of the line in world sapce
-        :param end_pos: End position of the line in world space
-        :param width: The width of the line, in pixels, defaults to 1
-        :return: A Rect that covers the area between the resulting line in screen space.
-        """
-        display = viewport.get_target_surface()
-
-        screen_start = CameraService.world_to_screen(start_pos, camera, viewport)
-        screen_end = CameraService.world_to_screen(end_pos, camera, viewport)
-
-        return pygame.draw.line(display, color, screen_start, screen_end, width)
-
-    @classmethod
     def draw_rect(
         cls,
         camera: Camera,
@@ -229,6 +200,23 @@ class CameraRenderer(Renderer):
             stop_angle,
             width,
         )
+
+    @classmethod
+    def draw_line(
+        cls,
+        camera: Camera,
+        viewport: Viewport,
+        color: ColorLike,
+        start_pos: Point,
+        end_pos: Point,
+        width: int = 1,
+    ) -> Rect:
+        display = viewport.get_target_surface()
+
+        screen_start = CameraService.world_to_screen(start_pos, camera, viewport)
+        screen_end = CameraService.world_to_screen(end_pos, camera, viewport)
+
+        return pygame.draw.line(display, color, screen_start, screen_end, width)
 
     @classmethod
     def render(cls, delta_time: float, camera: Camera, render_target: RenderTarget):
