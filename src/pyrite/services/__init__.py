1ed339cee557e0d8d2dd32cb0d69276bbcc31784
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-19 06:24:44 -0400

    Adds ServiceManager, implements fetch

--- src/pyrite/services/__init__.py
+++ src/pyrite/services/__init__.py
@@ -8,6 +8,24 @@ if TYPE_CHECKING:
     Service = TypeVar("Service", bound=ServiceType)
 
 
+class ServiceManager:
+
+    def __init__(self) -> None:
+        self.services: dict[type[Service], Service] = {}
+
+    def get_service(self, service_type: type[Service]) -> Service:
+        return self.services.get(service_type)
+
+    def start_service(self, service_type: type[Service]) -> Service:
+        # TODO Go through the rigamarole of figuring out how to get the specific
+        # subclass.
+        return service_type()
+
+
+# Keep one in memory so long as the module is loaded.
+service_manager = ServiceManager()
+
+
 def fetch(service_type: type[Service]) -> Service:
     """
     Gets the active service of the given type. If one does not exist, it will be
@@ -19,4 +37,6 @@ def fetch(service_type: type[Service]) -> Service:
     :param service_type: The type of the service to be fetched.
     :return: An instance of _service_type_
     """
-    pass
+    if (service := service_manager.get_service(service_type)) is None:
+        service = service_manager.start_service(service_type)
+    return service
