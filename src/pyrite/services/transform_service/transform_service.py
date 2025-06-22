d9c434099986f1c7c913fce6c71ec4ad8df60a5f
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-22 10:52:33 -0400

    Implements getter/setters for TransformService

--- src/pyrite/services/transform_service/transform_service.py
+++ src/pyrite/services/transform_service/transform_service.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 from abc import abstractmethod
 from typing import TYPE_CHECKING
 
-# from pygame import Vector2
+from weakref import WeakKeyDictionary
+
+from pygame import Vector2
 
 from ...transform import Transform
 from ...types.service import Service
@@ -99,4 +101,69 @@ class TransformService(Service):
 
 
 class DefaultTransformService(TransformService):
-    pass
+    def __init__(self) -> None:
+        self.world_transforms: WeakKeyDictionary[TransformComponent, Transform] = (
+            WeakKeyDictionary()
+        )
+        self.local_transforms: WeakKeyDictionary[TransformComponent, Transform] = (
+            WeakKeyDictionary()
+        )
+
+    def transfer(self, target_service: TransformService):
+        for component, transform in self.local_transforms.items():
+            target_service.initialize_component(component, transform)
+
+    def get_local(self, component: TransformComponent) -> Transform:
+        return self.local_transforms.get(component)
+
+    def get_local_position(self, component: TransformComponent) -> Point:
+        return self.local_transforms.get(component).position
+
+    def get_local_rotation(self, component: TransformComponent) -> float:
+        return self.local_transforms.get(component).rotation
+
+    def get_local_scale(self, component: TransformComponent) -> Point:
+        return self.local_transforms.get(component).scale
+
+    def set_local(self, component: TransformComponent, value: Transform):
+        self.local_transforms.update({component: value})
+
+    def set_local_position(self, component: TransformComponent, position: Point):
+        # dirty_components.add(component)
+        self.local_transforms.get(component).position = Vector2(position)
+
+    def set_local_rotation(self, component: TransformComponent, angle: Point):
+        # dirty_components.add(component)
+        self.local_transforms.get(component).rotation = angle
+
+    def set_local_scale(self, component: TransformComponent, scale: Point):
+        # dirty_components.add(component)
+        self.local_transforms.get(component).scale = Vector2(scale)
+
+    def get_world(self, component: TransformComponent) -> Transform:
+        return self.world_transforms.get(component)
+
+    def get_world_position(self, component: TransformComponent) -> Point:
+        return self.world_transforms.get(component).position
+
+    def get_world_rotation(self, component: TransformComponent) -> float:
+        return self.world_transforms.get(component).rotation
+
+    def get_world_scale(self, component: TransformComponent) -> Point:
+        return self.world_transforms.get(component).scale
+
+    def set_world(self, component: TransformComponent, value: Transform):
+        # TODO Force update local
+        self.world_transforms.update({component: value})
+
+    def set_world_position(self, component: TransformComponent, position: Point):
+        # TODO Force update local
+        self.world_transforms.get(component).position = Vector2(position)
+
+    def set_world_rotation(self, component: TransformComponent, angle: Point):
+        # TODO Force update local
+        self.world_transforms.get(component).rotation = angle
+
+    def set_world_scale(self, component: TransformComponent, scale: Point):
+        # TODO Force update local
+        self.world_transforms.get(component).scale = Vector2(scale)
