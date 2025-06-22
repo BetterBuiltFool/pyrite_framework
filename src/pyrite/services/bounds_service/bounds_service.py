aa1762327b3ff10466960cf0ed36428baf65e842
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-22 15:39:32 -0400

    Implements DefaultBoundsService

--- src/pyrite/services/bounds_service/bounds_service.py
+++ src/pyrite/services/bounds_service/bounds_service.py
@@ -1,16 +1,37 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from abc import abstractmethod
+from typing import TypeAlias, TYPE_CHECKING
+from weakref import WeakKeyDictionary
 
 from ...types.service import Service
 
 if TYPE_CHECKING:
-    pass
+    from ...types import CullingBounds, Renderable, Transform
+
+    BoundsData: TypeAlias = tuple[CullingBounds, Transform]
 
 
 class BoundsService(Service):
-    pass
+
+    @abstractmethod
+    def get(self, renderable: Renderable) -> BoundsData | tuple[None, None]:
+        pass
+
+    @abstractmethod
+    def set(self, renderable: Renderable, data: BoundsData):
+        pass
 
 
 class DefaultBoundsService(BoundsService):
-    pass
+
+    def __init__(self) -> None:
+        self._renderables: WeakKeyDictionary[Renderable, BoundsData] = (
+            WeakKeyDictionary()
+        )
+
+    def get(self, renderable: Renderable) -> BoundsData | tuple[None, None]:
+        return self._renderables.get(renderable, (None, None))
+
+    def set(self, renderable: Renderable, data: BoundsData):
+        self._renderables.update({renderable: data})
