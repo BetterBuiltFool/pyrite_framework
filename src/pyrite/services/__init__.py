e6317914d1010e2857a05382508b0e01cc733f0c
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-22 12:42:16 -0400

    Updates import for CameraServiceProvider

--- src/pyrite/services/__init__.py
+++ src/pyrite/services/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from .camera_service import CameraService  # noqa:F401
+from .camera_service import CameraServiceProvider as CameraService  # noqa:F401
 from .transform_service import TransformServiceProvider as TransformService  # noqa:F401
 
 if TYPE_CHECKING:
