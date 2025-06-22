54819be94deeb0eb30f7072021a0f10680bc59e2
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-22 12:40:53 -0400

    Adds TransformServiceProvider to package imports

--- src/pyrite/services/__init__.py
+++ src/pyrite/services/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from .camera_service import CameraService  # noqa:F401
+from .transform_service import TransformServiceProvider as TransformService  # noqa:F401
 
 if TYPE_CHECKING:
     pass
