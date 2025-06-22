8a89dc58446aec08fdaa7c19c79a44baf0d9ec9d
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-22 15:34:32 -0400

    Adds framework for BoundsService and default

---
+++ src/pyrite/services/bounds_service/bounds_service.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from ...types.service import Service
+
+if TYPE_CHECKING:
+    pass
+
+
+class BoundsService(Service):
+    pass
+
+
+class DefaultBoundsService(BoundsService):
+    pass
