195128bf6c5c37d4570cc7ced7d8e201bd543682
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-22 15:45:23 -0400

    Adds BoundsServiceProvider to services list

--- src/pyrite/services/__init__.py
+++ src/pyrite/services/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from .bounds_service import BoundsServiceProvider as BoundsService  # noqa:F401
 from .camera_service import CameraServiceProvider as CameraService  # noqa:F401
 from .transform_service import TransformServiceProvider as TransformService  # noqa:F401
 
