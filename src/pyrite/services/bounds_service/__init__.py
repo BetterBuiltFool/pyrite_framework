94763b5fda23549a2b4c5ca088e43b3a1a281328
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-22 15:34:56 -0400

    Adds references to BoundsService in provider

--- src/pyrite/services/bounds_service/__init__.py
+++ src/pyrite/services/bounds_service/__init__.py
@@ -4,9 +4,15 @@ from typing import TYPE_CHECKING
 
 from ...types.service import ServiceProvider
 
+from .bounds_service import BoundsService, DefaultBoundsService
+
 if TYPE_CHECKING:
     pass
 
 
 class BoundsServiceProvider(ServiceProvider):
-    pass
+    _service: BoundsService = DefaultBoundsService()
+
+    @classmethod
+    def hotswap(cls, service: BoundsService):
+        return super().hotswap(service)
