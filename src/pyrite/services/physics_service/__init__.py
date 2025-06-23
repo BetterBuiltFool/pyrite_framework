2e7d9f46232e655c09b75ae5b6fec0029d4f9095
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-22 22:38:24 -0400

    Adds PhysicsService imports to Provider

--- src/pyrite/services/physics_service/__init__.py
+++ src/pyrite/services/physics_service/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from .physics_service import PhysicsService, PymunkPhysicsService
 from ...types.service import ServiceProvider
 
 if TYPE_CHECKING:
@@ -9,4 +10,9 @@ if TYPE_CHECKING:
 
 
 class PhysicsServiceProvider(ServiceProvider):
-    pass
+    _service: PhysicsService = PymunkPhysicsService()
+
+    @classmethod
+    def hotswap(cls, service: PhysicsService):
+        cls._service.transfer(service)
+        cls._service = service
