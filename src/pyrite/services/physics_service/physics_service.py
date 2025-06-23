9d5dbdebcb918b3c1c164dbca7af56bec757359f
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-22 22:34:28 -0400

    Creates PhysicsService and Provider files

---
+++ src/pyrite/services/physics_service/physics_service.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from ...types.service import Service
+
+if TYPE_CHECKING:
+    pass
+
+
+class PhysicsService(Service):
+    pass
+
+
+class PymunkPhysicsService(PhysicsService):
+    pass
