d047cb8dccfaca57e5ec928b8866a5b891e2af7c
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-23 14:01:40 -0400

    Moves pymunk-specific function into PhysicsService

--- src/pyrite/physics/physics_system.py
+++ src/pyrite/physics/physics_system.py
@@ -1,52 +1,14 @@
 from __future__ import annotations
 
-from typing import Any, TYPE_CHECKING
+from typing import TYPE_CHECKING
 
 from ..types import System
 
-from .collider_component import ColliderComponent
 from ..transform import TransformComponent
 from ..services import PhysicsService
 
 if TYPE_CHECKING:
-    from pymunk import (
-        Arbiter,
-        Space,
-    )
-
-
-# Figure out where to put this so it doesn't cause circular imports
-# Track _bodies here? Would remove RigidbodyComponent import
-def post_solve(arbiter: Arbiter, space: Space, data: Any):
-    collider1, collider2 = get_collider_components(arbiter)
-    if arbiter.is_first_contact:
-        if collider1.compare_mask(collider2):
-            collider1.OnTouch(collider1, collider2)
-        if collider2.compare_mask(collider1):
-            collider2.OnTouch(collider2, collider1)
-    if collider1.compare_mask(collider2):
-        collider1.WhileTouching(collider1, collider2)
-    if collider2.compare_mask(collider1):
-        collider2.WhileTouching(collider2, collider1)
-
-
-def separate(arbiter: Arbiter, space: Space, data: Any):
-    collider1, collider2 = get_collider_components(arbiter)
-    if collider1.compare_mask(collider2):
-        collider1.OnSeparate(collider1, collider2)
-    if collider2.compare_mask(collider1):
-        collider2.OnSeparate(collider2, collider1)
-
-
-def get_collider_components(
-    arbiter: Arbiter,
-) -> tuple[ColliderComponent, ColliderComponent]:
-    shape1, shape2 = arbiter.shapes
-    body1 = shape1.body
-    body2 = shape2.body
-    owner1 = PhysicsService.get_owner_from_body(body1)
-    owner2 = PhysicsService.get_owner_from_body(body2)
-    return ColliderComponent.get(owner1), ColliderComponent.get(owner2)
+    pass
 
 
 class PhysicsSystem(System):
@@ -59,7 +21,6 @@ class PhysicsSystem(System):
 
     def __init__(self, physics_mult=1, enabled=True, order_index=0) -> None:
         super().__init__(enabled, order_index)
-        PhysicsService.set_component_handlers(post_solve=post_solve, separate=separate)
         self.physics_mult = physics_mult
 
     def const_update(self, timestep: float) -> None:
