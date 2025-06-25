9bfb6cb15e8e94a8811fa112463218f249908052
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-24 21:42:10 -0400

    Makes TransformProtocol use properties

--- src/pyrite/types/transform.py
+++ src/pyrite/types/transform.py
@@ -4,10 +4,26 @@ from typing import Protocol, runtime_checkable, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from pygame import Vector2
+    from pygame.typing import Point
 
 
 @runtime_checkable
 class TransformProtocol(Protocol):
-    position: Vector2
-    rotation: float
-    scale: Vector2
+
+    @property
+    def position(self) -> Vector2: ...
+
+    @position.setter
+    def position(self, new_position: Point): ...
+
+    @property
+    def rotation(self) -> float: ...
+
+    @rotation.setter
+    def rotation(self, angle: float): ...
+
+    @property
+    def scale(self) -> Vector2: ...
+
+    @scale.setter
+    def scale(scale, new_scale: Point): ...
