59ad5dfc6380a699f9bf25c207614f1ecbb6805b
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-28 09:56:17 -0400

    Removes invalid key check We've decided to have get_subsurface be failsafe, so it will never return None

--- tests/test_spritesheet.py
+++ tests/test_spritesheet.py
@@ -19,22 +19,10 @@ for i in range(8):
             test_image.fill((255, 0, 255), (i, j, 8, 8))
 
 
-class TestSpriteMap(SimpleSpriteMap):
-    """
-    Mockup sprite map that doesn't return the default rect on invalid key.
-    """
-
-    def get(self, key: tuple[int, int]) -> pygame.Rect:
-        if key is None:
-            return key
-        row, column = key
-        return self._map[row][column]
-
-
 class TestSpriteSheet(unittest.TestCase):
 
     def setUp(self) -> None:
-        sprite_map = TestSpriteMap(8, 8, (8, 8))
+        sprite_map = SimpleSpriteMap(8, 8, (8, 8))
         self.spritesheet = SpriteSheet(test_image, sprite_map, start_state=(0, 0))
 
     def test_get_subsurface(self):
@@ -48,13 +36,6 @@ class TestSpriteSheet(unittest.TestCase):
             # Since both surfaces are valid subsurfaces, their offsets should match.
             subsurface.get_offset(),
             self.spritesheet.get_subsurface(key).get_offset(),
-        )
-
-        invalid_key = None
-
-        self.assertIsNone(
-            # invalid key should cause None to be returned.
-            self.spritesheet.get_subsurface(invalid_key),
         )
 
 
