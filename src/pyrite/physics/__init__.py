4721af25098ca6ccb5e62bc471acc71f4621b9d8
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-28 21:35:38 -0400

    Adds init.py to physics module

---
+++ src/pyrite/physics/__init__.py
@@ -0,0 +1,3 @@
+from .collider_component import ColliderComponent  # noqa: F401
+from .kinematic_component import KinematicComponent  # noqa: F401
+from .rigidbody_component import RigidbodyComponent  # noqa: F401
