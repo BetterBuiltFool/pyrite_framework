9cd262532c2bbdc80b2cfa70d225bb4fca96940f
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-28 21:29:26 -0400

    Moves default display constants into constants.py

--- src/pyrite/constants.py
+++ src/pyrite/constants.py
@@ -3,8 +3,23 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import cffi
+import pygame
+
+from .core.display_settings import DisplaySettings
 
 if TYPE_CHECKING:
     pass
 
 COMPONENT_TYPE: int = 2 ** (cffi.FFI().sizeof("int") * 8 - 1) - 1
+
+DEFAULT_WINDOWED: DisplaySettings = DisplaySettings()
+"""
+Basic windowed display, 800x600
+"""
+DEFAULT_FULLSCREEN: DisplaySettings = DisplaySettings(
+    resolution=(0, 0), flags=pygame.FULLSCREEN
+)
+"""
+Fullscreen view, native resolution.
+Actual resolution supplied by pygame.
+"""
