d6435771edfd84fb8d2e04517924e3874b10341d
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-30 13:00:14 -0400

    Adds RendererProvider to types level imports

--- src/pyrite/types/__init__.py
+++ src/pyrite/types/__init__.py
@@ -9,7 +9,7 @@ from .debug_renderer import DebugRenderer  # noqa: F401
 from .entity import Entity  # noqa: F401
 from .projection import Projection  # noqa: F401
 from .renderable import Renderable  # noqa: F401
-from .renderer import Renderer  # noqa: F401
+from .renderer import Renderer, RendererProvider  # noqa: F401
 from .system import System  # noqa: F401
 from .transform import TransformLike  # noqa:F401
 from .view_bounds import CameraViewBounds  # noqa: F401
