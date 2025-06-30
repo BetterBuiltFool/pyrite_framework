8f5b29e5849ca8e5931a05ade4a6bbfd91afbc64
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-06-30 12:59:32 -0400

    Adds new RendererProvider class

--- src/pyrite/types/renderer.py
+++ src/pyrite/types/renderer.py
@@ -6,19 +6,42 @@ from typing import Generic, TypeVar, TYPE_CHECKING
 if TYPE_CHECKING:
     from . import Camera
 
-RenderableType = TypeVar("RenderableType")
+RenderableTypeT = TypeVar("RenderableTypeT")
 
 
-class Renderer(ABC, Generic[RenderableType]):
+class Renderer(ABC, Generic[RenderableTypeT]):
 
-    @classmethod
     @abstractmethod
-    def render(cls, delta_time: float, renderable: RenderableType, camera: Camera):
+    def render(self, delta_time: float, renderable: RenderableTypeT, camera: Camera):
         """
         Draws the renderable to the screen via the camera object.
 
         :param delta_time: Time passed since last frame
         :param renderable: The renderable item, of the type handled by the renderer.
         :param camera: A Camera object, used for determining screen space position.
+        """
+        pass
+
+
+ProvidedRendererT = TypeVar("ProvidedRendererT", bound=Renderer)
+
+
+class RendererProvider(ABC, Generic[ProvidedRendererT]):
+    """
+    Accessor class for a given renderer. Delegates methods to the renderer instance,
+    providing an access point to the users of that renderer while allowing the instance
+    to be swapped with variants at runtime.
+    """
+
+    _renderer: ProvidedRendererT
+
+    @classmethod
+    @abstractmethod
+    def hotswap(cls, renderer: ProvidedRendererT):
+        """
+        Changes over the renderer instance, and transfers and required data that is
+        shared between the two.
+
+        :param renderer: The new renderer instance.
         """
         pass
