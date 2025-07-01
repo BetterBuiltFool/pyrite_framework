3dc7484f762cd988d764a5013836f3d4f6e87e66
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-07-01 13:05:20 -0400

    Adds higher import for CameraRenderer

--- src/pyrite/rendering/__init__.py
+++ src/pyrite/rendering/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from .base_renderable import BaseRenderable as Renderable  # noqa: F401
+from .camera_renderer import CameraRendererProvider as CameraRenderer  # noqa: F401
 from .ortho_projection import OrthoProjection  # noqa: F401
 from .rect_bounds import RectBounds  # noqa: F401
 from .render_texture import RenderTexture, RenderTextureComponent  # noqa: F401
