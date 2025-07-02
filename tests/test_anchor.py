558f3795af6e04685145d78b2535551ed31f140a
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-07-02 13:12:07 -0400

    Parameterizes test_get_rect_center

--- tests/test_anchor.py
+++ tests/test_anchor.py
@@ -14,7 +14,13 @@ sys.path.append(str(pathlib.Path.cwd()))
 from src.pyrite.enum import Anchor  # noqa:E402
 
 anchor_center: Point = (0.5, 0.5)
+anchor_75x_5y: Point = (0.75, 0.5)
+anchor_5x_75y: Point = (0.5, 0.75)
+anchor_quarter: Point = (0.25, 0.25)
+
 rect_size: RectLike = (0, 0, 8, 4)
+
+default_scale: Point = (1, 1)
 
 
 class TestAnchor(unittest.TestCase):
@@ -22,9 +28,9 @@ class TestAnchor(unittest.TestCase):
     def test_get_center_offset(self):
         test_params: dict[str, tuple[Point, RectLike, Point]] = {
             "Center Anchor": (anchor_center, rect_size, (0, 0)),
-            "Center X, 3/4 Y": ((0.5, 0.75), rect_size, (0, 1)),
-            "3/4 X, Center Y": ((0.75, 0.5), rect_size, (2, 0)),
-            "1/4 X, 1/4 Y": ((0.25, 0.25), rect_size, (-2, -1)),
+            "Center X, 3/4 Y": (anchor_5x_75y, rect_size, (0, 1)),
+            "3/4 X, Center Y": (anchor_75x_5y, rect_size, (2, 0)),
+            "1/4 X, 1/4 Y": (anchor_quarter, rect_size, (-2, -1)),
         }
 
         for index, (case, params) in enumerate(test_params.items()):
@@ -38,63 +44,56 @@ class TestAnchor(unittest.TestCase):
                 self.assertEqual(pivot, expected)
 
     def test_get_rect_center(self):
-        # Case: (Center X, Center Y), 0 C
-        rect = Rect(0, 0, 8, 4)
-        anchor = Anchor((0.5, 0.5))
-        angle = 0
+        test_params: dict[str, tuple[Point, float, Point, Point]] = {
+            "Case: (Center X, Center Y), 0 C": (
+                anchor_center,
+                0,
+                default_scale,
+                (6, -2),
+            ),
+            "Case: (3/4 X, Center Y), 0 C": (
+                anchor_75x_5y,
+                0,
+                default_scale,
+                (4, -2),
+            ),
+            "Case: (3/4 X, Center Y), 90 C": (
+                anchor_75x_5y,
+                90,
+                default_scale,
+                (6, -4),
+            ),
+            "Case: (3/4 X, Center Y), -90 C": (
+                anchor_75x_5y,
+                -90,
+                default_scale,
+                (6, 0),
+            ),
+            "Case: (3/4 X, Center Y), 180 C": (
+                anchor_75x_5y,
+                180,
+                default_scale,
+                (8, -2),
+            ),
+            "Case: (3/4 X, Center Y), 0 C, (2,2) scale": (
+                anchor_75x_5y,
+                0,
+                (2, 2),
+                (2, -2),
+            ),
+        }
 
-        new_center = anchor.get_rect_center(rect, (6, -2), angle)
+        for index, (case, params) in enumerate(test_params.items()):
+            with self.subTest(case, i=index):
+                rect = Rect(rect_size)
+                anchor = Anchor(params[0])
+                angle = params[1]
+                scale = params[2]
+                expected = Vector2(params[3])
 
-        expected = Vector2(6, -2)
+                new_center = anchor.get_rect_center(rect, (6, -2), angle, scale)
 
-        self.assertEqual(new_center, expected)
-
-        # Case: (3/4 X, Center Y), 0 C
-        anchor = Anchor((0.75, 0.5))
-        angle = 0
-
-        new_center = anchor.get_rect_center(rect, (6, -2), angle)
-
-        expected = Vector2(4, -2)
-
-        self.assertEqual(new_center, expected)
-
-        # Case: (3/4 X, Center Y), 90 C
-        angle = 90
-
-        new_center = anchor.get_rect_center(rect, (6, -2), angle)
-
-        expected = Vector2(6, -4)
-
-        self.assertEqual(new_center, expected)
-
-        # Case: (3/4 X, Center Y), -90 C
-        angle = -90
-
-        new_center = anchor.get_rect_center(rect, (6, -2), angle)
-
-        expected = Vector2(6, 0)
-
-        self.assertEqual(new_center, expected)
-
-        # Case: (3/4 X, Center Y), 180 C
-        angle = 180
-
-        new_center = anchor.get_rect_center(rect, (6, -2), angle)
-
-        expected = Vector2(8, -2)
-
-        self.assertEqual(new_center, expected)
-
-        # Case: (3/4 X, Center Y), 0 C, (2,2) scale
-        angle = 0
-        scale = (2, 2)
-
-        new_center = anchor.get_rect_center(rect, (6, -2), angle, scale)
-
-        expected = Vector2(2, -2)
-
-        self.assertEqual(new_center, expected)
+                self.assertEqual(new_center, expected)
 
 
 if __name__ == "__main__":
