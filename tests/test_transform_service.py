68b2bbd60fdc11a6850e5105a4813fa5827f0343
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-07-23 13:26:46 -0400

    Adds test for initialize_component.

--- tests/test_transform_service.py
+++ tests/test_transform_service.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 import unittest
 
-# from pyrite.transform import Transform, TransformComponent
+from pyrite.transform import TransformComponent
+from pyrite.services import TransformService
 from pyrite.services.transform_service.transform_service import DefaultTransformService
 
 
@@ -14,6 +15,22 @@ class TestDefaultTransformService(unittest.TestCase):
 
     def setUp(self) -> None:
         self.transform_service = DefaultTransformService()
+
+        # Ensure that when test objects are created, we know what service object they
+        # are dealing with.
+        TransformService.hotswap(self.transform_service)
+
+    def test_initialize_component(self) -> None:
+        root_component = TransformComponent(Empty())
+
+        self.assertIn(root_component, self.transform_service.world_transforms)
+        self.assertIn(root_component, self.transform_service.local_transforms)
+        self.assertIn(root_component, self.transform_service.dirty_components)
+        self.assertIn(root_component, self.transform_service.transform_nodes)
+
+        root_node = self.transform_service.transform_nodes[root_component]
+
+        self.assertIn(root_node, self.transform_service.root_transforms)
 
 
 if __name__ == "__main__":
