94f6ff0f275e54e8887586b97dcdf8c6c1067187
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-07-24 13:58:29 -0400

    Condenses TransformSystem ABC was no longer useful, since there are no more abstract methods to define.

--- src/pyrite/transform/transform_system.py
+++ src/pyrite/transform/transform_system.py
@@ -1,81 +1,20 @@
 from __future__ import annotations
 
-from abc import abstractmethod
 from typing import TYPE_CHECKING
 
 from ..services import TransformService
 from ..systems import System
-from .transform import Transform
 
 
 if TYPE_CHECKING:
-    from .transform_component import TransformComponent
+    pass
 
 
 class TransformSystem(System):
-
-    # @staticmethod
-    # def to_world(transform: Transform, context: Transform) -> Transform:
-    #     """
-    #     :param transform: A Transform in local space
-    #     :param context: A Transform representing the local space of _transform_,
-    #         in world space
-    #     :return: Equivalent to _transform_, in world space.
-    #     """
-    #     pass
-
-    # @staticmethod
-    # def to_local(transform: Transform, context: Transform) -> Transform:
-    #     """
-    #     :param transform: A Transform in world space
-    #     :param context: A Transform representing the local space _transform_ is being
-    #         converted to, in world space
-    #     :return: Equivalent to _transform_, in local space.
-    #     """
-    #     pass
-
-    @staticmethod
-    @abstractmethod
-    def convert_to_world(transform: TransformComponent) -> Transform:
-        """
-        Converts the transform component into world space, with context defined by the
-        system implementation.
-
-        :param transform: A TransformComponent object.
-        :return: A Transform that represents the world space value of _transform_.
-        """
-        pass
-
-    @staticmethod
-    @abstractmethod
-    def convert_to_local(transform: Transform) -> Transform:
-        """
-        Converts the transform into local space, with context defined by the system
-        implementation.
-
-        :param transform: A Transform object in world space.
-        :return: A Transform that represents _transform_ in local space.
-        """
-        pass
-
-
-class DefaultTransformSystem(TransformSystem):
     """
-    Simple TransformSystem that doesn't differentiate between world and local
-    transforms.
+    System for ensuring that TransformComponent world values are up to date for
+    rendering.
     """
-
-    @staticmethod
-    def convert_to_world(transform: TransformComponent) -> Transform:
-        if not (parent := TransformService.get_relative_of(transform)):
-            # We are dealing with a root component
-            return transform.raw().copy()
-
-        return parent.world() * transform
-
-    @staticmethod
-    def convert_to_local(transform: Transform) -> Transform:
-        return transform
 
     def pre_render(self, delta_time: float) -> None:
         for component in TransformService.traverse_transforms():
@@ -90,7 +29,7 @@ class DefaultTransformSystem(TransformSystem):
             TransformService.clean(component)
 
 
-_default_transform_system: type[TransformSystem] = DefaultTransformSystem
+_default_transform_system: type[TransformSystem] = TransformSystem
 
 
 def get_default_transform_system_type() -> type[TransformSystem]:
