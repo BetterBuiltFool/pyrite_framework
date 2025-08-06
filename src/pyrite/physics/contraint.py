9da6da0e7db48d9faf98cf0fa6b49150f8f55738
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-08-06 12:50:03 -0400

    Adds init, removes abstractmethod Might remove ABC too, depends on how this develops. Constraint has some default behaviors, which rely on attributes that are used across all subtypes, so we're adding an init to create those attributes.

--- src/pyrite/physics/contraint.py
+++ src/pyrite/physics/contraint.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
+from abc import ABC  # , abstractmethod
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -13,18 +13,20 @@ class Constraint(ABC):
     physics engine.
     """
 
+    def __init__(self, body_a: RigidbodyComponent, body_b: RigidbodyComponent) -> None:
+        self._a = body_a
+        self._b = body_b
+
     @property
-    @abstractmethod
     def a(self) -> RigidbodyComponent:
         """
         The first of the two constrained rigidbodies.
         """
-        pass
+        return self._a
 
     @property
-    @abstractmethod
     def b(self) -> RigidbodyComponent:
         """
         The second of the two constrained rigidbodies.
         """
-        pass
+        return self._b
