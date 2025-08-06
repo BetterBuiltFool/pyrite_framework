32dfef9d554647c264ced8b11659b66a2e89864c
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-08-06 13:28:30 -0400

    Adds damping property to DampedRotarySpring

--- src/pyrite/physics/contraint.py
+++ src/pyrite/physics/contraint.py
@@ -23,6 +23,20 @@ class DampedRotarySpring(Constraint[pymunk.DampedRotarySpring]):
         damping: float,
     ) -> None:
         super().__init__(body_a, body_b)
+        # TODO Convert rest_angle to radians. We want to be user-facing values to be in
+        # degrees to match other places.
         self._constraint = pymunk.DampedRotarySpring(
             body_a.body, body_b.body, rest_angle, stiffness, damping
         )
+
+    @property
+    def damping(self) -> float:
+        """
+        Controls the apparent "softness" of the spring.
+        Higher values result in stiffer springs.
+        """
+        return self._constraint.damping
+
+    @damping.setter
+    def damping(self, damping: float) -> None:
+        self._constraint.damping = damping
