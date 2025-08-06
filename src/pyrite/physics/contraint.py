2067bc2a54f496c73b55bbb801c75f82eb6f455e
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-08-06 12:55:50 -0400

    Adds activate-bodies method

--- src/pyrite/physics/contraint.py
+++ src/pyrite/physics/contraint.py
@@ -4,6 +4,7 @@ from abc import ABC  # , abstractmethod
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from pymunk import Constraint as PymunkConstraint
     from .rigidbody_component import RigidbodyComponent
 
 
@@ -16,6 +17,7 @@ class Constraint(ABC):
     def __init__(self, body_a: RigidbodyComponent, body_b: RigidbodyComponent) -> None:
         self._a = body_a
         self._b = body_b
+        self._constraint: PymunkConstraint
 
     @property
     def a(self) -> RigidbodyComponent:
@@ -30,3 +32,7 @@ class Constraint(ABC):
         The second of the two constrained rigidbodies.
         """
         return self._b
+
+    def activate_bodies(self) -> None:
+        """ """
+        self._constraint.activate_bodies()
