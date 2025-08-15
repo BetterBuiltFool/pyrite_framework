3da3b718032d59d6f520afc96e4286318d70f53a
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-08-15 12:49:57 -0400

    Adds Segment shape

--- src/pyrite/physics/shapes.py
+++ src/pyrite/physics/shapes.py
@@ -91,3 +91,66 @@ class Polygon(Shape[pymunk.Poly]):
     ) -> Polygon:
         verts = [rect.topleft, rect.topright, rect.bottomright, rect.bottomleft]
         return Polygon(collider, verts, None, radius)
+
+
+class Segment(Shape[pymunk.Segment]):
+
+    def __init__(
+        self,
+        collider: ColliderComponent | None,
+        a: Point,
+        b: Point,
+        radius: float,
+    ) -> None:
+        super().__init__(collider)
+
+        self._shape = pymunk.Segment(None, point_to_tuple(a), point_to_tuple(b), radius)
+
+    @property
+    def a(self) -> Vector2:
+        """
+        Position of the first endpoint of the segment.
+        """
+        return Vector2(self._shape.a)
+
+    @a.setter
+    def a(self, a: Point) -> None:
+        self._shape.a = point_to_tuple(a)
+
+    @property
+    def b(self) -> Vector2:
+        """
+        Position of the second endpoint of the segment.
+        """
+        return Vector2(self._shape.b)
+
+    @b.setter
+    def b(self, b: Point) -> None:
+        self._shape.b = point_to_tuple(b)
+
+    @property
+    def normal(self) -> Vector2:
+        """
+        The calculated normal of this segment.
+        """
+        return Vector2(self._shape.normal)
+
+    def set_neighbors(
+        self, prev: Segment | None = None, next: Segment | None = None
+    ) -> None:
+        """
+        Sets the neighbors to avoid collision with 'cracks' between segments.
+        """
+        prev_point = prev.b if prev else self.a
+        next_point = next.a if next else self.b
+        self.set_neighbor_points(prev_point, next_point)
+
+    def set_neighbor_points(self, prev: Point, next: Point) -> None:
+        """
+        Sets neighbors based directly on the endpoints provided.
+
+        :param prev: A point in space that is the second endpoint of the previous
+            neighbor.
+        :param next: A point in space that is the first endpoint of the next neighbor.
+        """
+        self._shape.set_neighbors(point_to_tuple(prev), point_to_tuple(next))
