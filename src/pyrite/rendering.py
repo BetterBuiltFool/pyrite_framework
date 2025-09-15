17ec751dc07770d9486c9cda8553ff2893e42c07
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-09-15 13:37:54 -0400

    Adds rendering module

---
+++ src/pyrite/rendering.py
@@ -0,0 +1,16 @@
+import pyrite._rendering.base_renderable
+import pyrite._rendering.camera_renderer
+import pyrite._rendering.ortho_projection
+import pyrite._rendering.rect_bounds
+import pyrite._rendering.render_texture
+import pyrite._rendering.sprite_renderer
+import pyrite._rendering.viewport
+
+Renderable = pyrite._rendering.base_renderable.BaseRenderable
+CameraRenderer = pyrite._rendering.camera_renderer.CameraRendererProvider
+OrthoProjection = pyrite._rendering.ortho_projection.OrthoProjection
+RectBounds = pyrite._rendering.rect_bounds.RectBounds
+RenderTexture = pyrite._rendering.render_texture.RenderTexture
+RenderTextureComponent = pyrite._rendering.render_texture.RenderTextureComponent
+SpriteRenderer = pyrite._rendering.sprite_renderer.SpriteRendererProvider
+Viewport = pyrite._rendering.viewport.Viewport
