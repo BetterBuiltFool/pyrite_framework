8cb342c7b5681280a137c70a2ef7c6f08e22a9b5
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-09-19 12:53:54 -0400

    Fixes all other imports in pyrite.__init__ All now use the new-style imports.

--- src/pyrite/__init__.py
+++ src/pyrite/__init__.py
@@ -1,27 +1,38 @@
-from .core.display_settings import DisplaySettings  # noqa: F401
-from .core.game_data import GameData  # noqa: F401
-from .core.rate_settings import RateSettings  # noqa: F401
-from .game import (  # noqa: F401
-    Game,
-    AsyncGame,
-    get_game_instance,
-)
-
+import pyrite.core.display_settings
+import pyrite.core.game_data
+import pyrite.core.rate_settings
+import pyrite.game
 import pyrite._camera.camera
 import pyrite._camera.chase_camera
-from .entity import Entity  # noqa:F401
-from .enum import RenderLayers, AnchorPoint  # noqa: F401
-from .physics import (  # noqa: F401
-    ColliderComponent,
-    KinematicComponent,
-    RigidbodyComponent,
-)
+import pyrite._entity.entity
+import pyrite.enum
+import pyrite._physics.collider_component
+import pyrite._physics.kinematic_component
+import pyrite._physics.rigidbody_component
 import pyrite._rendering.base_renderable
 import pyrite._sprite.sprite
 import pyrite._sprite.spritesheet
 import pyrite._systems.base_system
 import pyrite._transform.transform
 import pyrite._transform.transform_component
+
+DisplaySettings = pyrite.core.display_settings.DisplaySettings
+GameData = pyrite.core.game_data.GameData
+RateSettings = pyrite.core.rate_settings.RateSettings
+
+Game = pyrite.game.Game
+AsyncGame = pyrite.game.AsyncGame
+get_game_instance = pyrite.game.get_game_instance
+
+Entity = pyrite._entity.entity.BaseEntity
+
+RenderLayers = pyrite.enum.RenderLayers
+AnchorPoint = pyrite.enum.AnchorPoint
+AbsoluteAnchor = pyrite.enum.AbsoluteAnchor
+
+ColliderComponent = pyrite._physics.collider_component.ColliderComponent
+KinematicComponent = pyrite._physics.kinematic_component.KinematicComponent
+RigidbodyComponent = pyrite._physics.rigidbody_component.RigidbodyComponent
 
 Camera = pyrite._camera.camera.Camera
 ChaseCamera = pyrite._camera.chase_camera.ChaseCamera
