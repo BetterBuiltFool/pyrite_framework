10a4682a50d0693f28fa0027b64b9d9e897b7830
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-10-02 13:19:24 -0400

    Removes delta_time param from BaseEntity

--- src/pyrite/_entity/entity.py
+++ src/pyrite/_entity/entity.py
@@ -54,13 +54,13 @@ class BaseEntity(Entity):
     def on_disable(self):
         pass
 
-    def pre_update(self, delta_time: float) -> None:
+    def pre_update(self) -> None:
         pass
 
-    def update(self, delta_time: float) -> None:
+    def update(self) -> None:
         pass
 
-    def post_update(self, delta_time: float) -> None:
+    def post_update(self) -> None:
         pass
 
     def const_update(self, timestep: float) -> None:
