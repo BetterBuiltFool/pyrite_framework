558eb8022f5caf371a358fe9dea5ae71493b564f
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-10-03 13:29:36 -0400

    Adds fixed_time_step method to time module

--- src/pyrite/time.py
+++ src/pyrite/time.py
@@ -1,4 +1,5 @@
 _dt: float = 0
+_fxt: float = 0
 
 
 def delta_time() -> float:
@@ -6,3 +7,10 @@ def delta_time() -> float:
     Returns the time passed since the previous frame.
     """
     return _dt
+
+
+def fixed_time_step() -> float:
+    """
+    Returns the length of the fixed time step used in const_update.
+    """
+    return _fxt
