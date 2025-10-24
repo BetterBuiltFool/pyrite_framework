0511a524f55a8aa7c34801c8f8656db9e202b718
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-10-24 12:36:17 -0400

    Adds eye and ndc conversion methods to Projection

--- src/pyrite/_types/projection.py
+++ src/pyrite/_types/projection.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from pygame import Rect
+    from pygame import Rect, Vector3
 
 
 class Projection(ABC):
@@ -40,4 +40,23 @@ class Projection(ABC):
     def z_depth(self) -> float:
         """
         Total depth of the projected area.
+        """
+
+    @abstractmethod
+    def ndc_to_eye(self, ndc_coords: Vector3) -> Vector3:
+        """
+        Converts Normalized Device Coordinates into eye coordinates for the projection.
+
+        :param ndc_coords: A 3D point in NDC space.
+        :return: The equivalent point in the projection's local space.
+        """
+
+    @abstractmethod
+    def eye_to_ndc(self, eye_coords: Vector3) -> Vector3:
+        """
+        Converts a point in the projection's local space into Normalized Device
+        Coordinates.
+
+        :param eye_coords: A 3D point in the projection's local space.
+        :return: The equivalent point in NDC space.
         """
