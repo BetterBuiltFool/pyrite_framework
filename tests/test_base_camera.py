f3b55b24f9be8820d8ee1f8854dd9a0b7bc69212
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-10-28 13:07:12 -0400

    Added type aliases to test base camera This makes the test more readable by ensuring the reader knows the purpose of the data.

--- tests/test_base_camera.py
+++ tests/test_base_camera.py
@@ -12,6 +12,9 @@ from pyrite._rendering.viewport import Viewport
 if TYPE_CHECKING:
     from pygame.typing import Point
 
+    type ScreenPoint = Point
+    type LocalPoint = Point
+
 CAMERA = BaseCamera(OrthoProjection((-50, -50, 100, 100)))
 Viewport.DEFAULT._update_display_rect((100, 100))
 
@@ -23,7 +26,7 @@ TOPLEFT_QUADRANT_VIEW._update_display_rect((100, 100))
 class TestBaseCamera(unittest.TestCase):
 
     def test_get_mouse_position(self) -> None:
-        params: dict[str, tuple[BaseCamera, Viewport, Point, Point]] = {
+        params: dict[str, tuple[BaseCamera, Viewport, ScreenPoint, LocalPoint]] = {
             "Centered mouse": (CAMERA, Viewport.DEFAULT, (50, 50), (0, 0)),
             "Topleft mouse": (CAMERA, Viewport.DEFAULT, (0, 0), (-50, 50)),
             "Bottomright mouse": (CAMERA, Viewport.DEFAULT, (100, 100), (50, -50)),
