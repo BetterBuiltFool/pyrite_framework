1c4f85de1a0bf3675b088c17ffbf2060c19d12bb
BetterBuiltFool <betterbuiltfool@gmail.com>
2025-11-17 13:21:06 -0500

    Adds Cuboid init for Cuboid and Rects Allows Rects and a point to be used to specify construction of a cuboid.

--- src/pyrite/cuboid.py
+++ src/pyrite/cuboid.py
@@ -2,8 +2,16 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from pygame import Rect
+
 if TYPE_CHECKING:
-    pass
+    from pygame.typing import RectLike, Point
+    from pyrite.types import CubeLike
+
+    RectPoint = tuple[RectLike, Point]
+    RectBased = tuple[RectLike, float, float]
+
+    CuboidTuple = tuple[float, float, float, float, float, float]
 
 
 class Cuboid:
@@ -17,16 +25,64 @@ class Cuboid:
 
     def __init__(
         self,
-        left: float,
-        top: float,
-        front: float,
-        width: float,
-        height: float,
-        depth: float,
+        left: float | CubeLike,
+        top: float = 0,
+        front: float = 0,
+        width: float = 0,
+        height: float = 0,
+        depth: float = 0,
     ) -> None:
-        self.left = left
+        if isinstance(left, Cuboid):
+            left, top, front, width, height, depth = self._deconstruct_cuboid(left)
+        if isinstance(left, tuple):
+            left, top, front, width, height, depth = self._deconstruct_tuple(left)
+
+        self.left: float = left
         self.top = top
         self.front = front
         self.width = width
         self.height = height
         self.depth = depth
+
+    @staticmethod
+    def _deconstruct_cuboid(
+        cuboid: Cuboid,
+    ) -> CuboidTuple:
+        return (
+            cuboid.left,
+            cuboid.top,
+            cuboid.front,
+            cuboid.width,
+            cuboid.height,
+            cuboid.depth,
+        )
+
+    @staticmethod
+    def _deconstruct_tuple(cuboid: RectPoint | RectBased) -> CuboidTuple:
+        if len(cuboid) == 2:
+            front, depth = cuboid[1]
+        elif len(cuboid) == 3:
+            front, depth = cuboid[1], cuboid[2]
+        else:
+            front, depth = 0  # temp, should raise an error.
+        try:
+            rect = Rect(cuboid[0])
+        except ValueError:
+            # Invalid Rectlike.
+            rect = Rect(0, 0, 0, 0)  # temp, should raise an error.
+
+        top, left = rect.topleft
+        width, height = rect.size
+        return left, top, front, width, height, depth
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, Cuboid):
+            return False
+        return (
+            value.top == self.top
+            and value.left == self.left
+            and value.front == self.front
+            and value.width == self.width
+            and value.height == self.height
+            and value.depth == self.depth
+        )
