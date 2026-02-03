2e6695cbaadaf06ac323a895ca97994015f6cc7f
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-02-03 13:03:56 -0500

    Convert System type to Protocol System type is now a protocol describing BaseSystem's methods and members BaseSystem no longer inherits from System.

--- src/pyrite/_systems/base_system.py
+++ src/pyrite/_systems/base_system.py
@@ -5,14 +5,15 @@ from typing import TYPE_CHECKING
 
 from pyrite.core.system_manager import SystemManager
 from pyrite.core.enableable import Enableable
-from pyrite.events import OnEnable, OnDisable
-from pyrite._types.system import System
+
+# from pyrite.events import OnEnable, OnDisable
+# from pyrite._types.system import System
 
 if TYPE_CHECKING:
-    pass
+    from pygame import Event
 
 
-class BaseSystem(System, Enableable[SystemManager], manager=SystemManager):
+class BaseSystem(Enableable[SystemManager], manager=SystemManager):
     """
     Base class for all systems that perform actions on components.
 
@@ -31,11 +32,63 @@ class BaseSystem(System, Enableable[SystemManager], manager=SystemManager):
             priority going down as value increases, but negative numbers are
             approximately distance from last, defaults to 0 (Tie for first)
         """
-        self.OnEnable = OnEnable(self)
-        self.OnDisable = OnDisable(self)
-        self._enabled = None
-        self.enabled = enabled
+        super().__init__(enabled)
+        # self.OnEnable = OnEnable(self)
+        # self.OnDisable = OnDisable(self)
+        # self._enabled = None
+        # self.enabled = enabled
         self.order_index = order_index
 
     def __init_subclass__(cls, **kwds) -> None:
         return super().__init_subclass__(manager=SystemManager, **kwds)
+
+    def pre_update(self) -> None:
+        """
+        A method that is called during the pre_update phase.
+        Will always be called before update.
+        """
+        ...
+
+    def update(self) -> None:
+        """
+        A method that is called during the main update phase.
+        Most behaviour should happen here.
+        """
+        ...
+
+    def post_update(self) -> None:
+        """
+        A method that is called during the post_update phase.
+        Will always be called after update.
+        """
+        ...
+
+    def const_update(self) -> None:
+        """
+        A method that is called during the const_update phase.
+        Useful for behavior that is sensitive to time fluctuations,
+        such as physics or AI.
+
+        const_update is called before any other update methods.
+
+        const_update may be called any number of times per frame,
+        depending on timestep length.
+        """
+        ...
+
+    def pre_render(self) -> None:
+        """
+        A method that is called immediately before the render phase.
+        Used by TransformServices to ensure transforms are properly updated just prior
+        to being used to display them.
+        """
+        ...
+
+    def on_event(self, event: Event):
+        """
+        An event hook. Events will be passed to the entity when it's enabled, and can
+        be handled here.
+
+        :param event: A pygame event.
+        """
+        ...
--- src/pyrite/_types/system.py
+++ src/pyrite/_types/system.py
@@ -1,17 +1,20 @@
 from __future__ import annotations
-from abc import ABC
 
-import typing
+from typing import Protocol, TYPE_CHECKING
 
-if typing.TYPE_CHECKING:
+if TYPE_CHECKING:
     from pygame import Event
 
+    from pyrite.events import OnEnable as OnEnableEvent, OnDisable as OnDisableEvent
 
-class System(ABC):
+
+class System(Protocol):
     """
     Base class for all systems.
     """
 
+    OnEnable: OnEnableEvent
+    OnDisable: OnDisableEvent
     order_index: int
 
     def pre_update(self) -> None:
@@ -19,21 +22,21 @@ class System(ABC):
         A method that is called during the pre_update phase.
         Will always be called before update.
         """
-        pass
+        ...
 
     def update(self) -> None:
         """
         A method that is called during the main update phase.
         Most behaviour should happen here.
         """
-        pass
+        ...
 
     def post_update(self) -> None:
         """
         A method that is called during the post_update phase.
         Will always be called after update.
         """
-        pass
+        ...
 
     def const_update(self) -> None:
         """
@@ -46,7 +49,7 @@ class System(ABC):
         const_update may be called any number of times per frame,
         depending on timestep length.
         """
-        pass
+        ...
 
     def pre_render(self) -> None:
         """
@@ -54,6 +57,7 @@ class System(ABC):
         Used by TransformServices to ensure transforms are properly updated just prior
         to being used to display them.
         """
+        ...
 
     def on_event(self, event: Event):
         """
@@ -62,4 +66,18 @@ class System(ABC):
 
         :param event: A pygame event.
         """
-        pass
+        ...
+
+    @property
+    def enabled(self) -> bool: ...
+
+    @enabled.setter
+    def enabled(self, enabled: bool) -> None: ...
+
+    def on_preenable(self): ...
+
+    def on_enable(self): ...
+
+    def on_predisable(self): ...
+
+    def on_disable(self): ...
